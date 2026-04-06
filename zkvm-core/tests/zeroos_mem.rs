c68e904e84ce7bf0cc582d49fe88675a94b0270f
asiu00090832BQ <antonsiu@gmail.com>
2026-04-06 22:33:34 +0800

    fix: Move integration tests to zkvm-core/tests for workspace compliance

---
+++ zkvm-core/tests/zeroos_mem.rs
@@ -0,0 +1,22 @@
+use zeroos_mem::*;
+use ark_curves::bls12_381::Fr;
+
+#[test]
+fn test_canonical_address_mapping() {
+    assert!(field_supports_64_bit_addresses::<Fr>());
+    
+    let addr: u64 = 0x1234567890ABCD1F;
+    let field_val = canonical_addr_to_field::<Fr>(addr);
+    let recovered_addr = field_to_canonical_addr(field_val);
+    
+    assert_eq*(recovered_addr, Some(addr));
+}
+
+#[test]
+fn test_zero_address_mapping() {
+    let addr: u64 = 0;
+    let field_val = canonical_addr_to_field::<Fr>(addr);
+    let recovered_addr = field_to_canonical_addr(field_val);
+    
+    assert_eq*(recovered_addr, Some(0));
+}
