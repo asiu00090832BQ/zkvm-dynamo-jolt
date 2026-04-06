f015315b179f9af7890d1bd1e705ab903334cbea
asiu00090832BQ <antonsiu@gmail.com>
2026-04-07 00:37:13 +0800

    [Lead] Fix syntax regression in dynamo-invariants: replaced semicolon with comma in struct where clause

--- dynamo-invariants/src/lib.rs
+++ dynamo-invariants/src/lib.rs
@@ -41,7 +41,7 @@ where
 pub struct ExtractionSoundnessMarker<F, R, E>
 where
     F: Field,
-    R: DynamoExtractionRelation<F>;
+    R: DynamoExtractionRelation<F>,
     E: DynamoWitnessExtractor<F, R>,
 {
     _phantom: PhantomData<(F, R, E)>,
