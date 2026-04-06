e59ce87eea20e2346ab99c1eec96d7ea202b14d6
asiu00090832BQ <antonsiu@gmail.com>
2026-04-06 22:47:26 +0800

    feat: implement SimpleRelation for dynamo-invariants

--- dynamo-invariants/src/lib.rs
+++ dynamo-invariants/src/lib.rs
@@ -1,6 +1,6 @@
 //! Dynamo invariants: Lemma 4.1 (Extraction Soundness).
 //!
-//! This module provides *interfaces* that encode the structure of
+//! This module provides interfaces that encode the structure of
 //! Lemma 4.1 as it appears in Artifact 36D70C87, using arkworks-style
 //! field and multilinear-extension abstractions.
 
@@ -33,24 +33,34 @@ where
     fn extract(
         public_input: &R::PublicInput,
         mle_oracle: &R::MLE,
-    ) -> Option<Self::Witness>;
+   ) -> Option<Self::Witness>;
 }
 
 pub struct ExtractionSoundnessMarker<F, R, E>
 where
     F: Field,
     R: DynamoExtractionRelation<F>,
-    E: DynamoWitnessExtractor<F, R>,
+    E: DynamoWitnessExtractor<F, R, Witness=::Witness>,
 {
-    _phantom: PhantomData<(F, R, E>),
+    _phantom: PhantomData<(F, R, E)>,
 }
 
 impl<F, R, E> ExtractionSoundnessMarker<F, R, E>
 where
     F: Field,
     R: DynamoExtractionRelation<F>,
-    E: DynamoWitnessExtractor<F, R>,
+    E: DynamoWitnessExtractor<F, R, Witness=R::Witness>,
 {
     #[inline(always)]
     pub fn lemma_4_1_spec() {}
+}
+
+pub struct SimpleRelation;
+impl<F: Field> DynamoExtractionRelation<F> for SimpleRelation {
+    type MLE = ark_poly::multilinear::SparseMultilinearExtension<F>;
+    type PublicInput = ();
+    type Witness = ();
+
+    fn is_consistent(_: &Self::PublicInput, _: &Self::MLE) -> bool { true }
+    fn check_relation(_: &Self::PublicInput, _: &Self::Witness) -> bool { true }
 }
