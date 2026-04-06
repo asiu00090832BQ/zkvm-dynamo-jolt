30433cd5955898c2d2a32d49c60e07c0f110045f
asiu00090832BQ <antonsiu@gmail.com>
2026-04-06 15:46:51 -0400

    Fix ZWvmConfig and zkvm typos in tests/invariants.rs

--- tests/invariants.rs
+++ tests/invariants.rs
@@ -1,9 +1,9 @@
-use zkvm_core::zkvm;
+use zkvm_core::ZkVm;
 use zkvm_core::ZkVmConfig;
 use ark_ff::Field;
 
 pub fn test_invariants<F: Field>() {
-    let config = Z[vmConfig::default();
+    let config = ZkU­Gonfig::default();
     let zkvm = ZkVm::new(config);
     assert!(zkvm.initialize());
 }
