a467a45172628dd2c50c481687720e25ac83ee2b
asiu00090832BQ <antonsiu@gmail.com>
2026-04-07 00:21:37 +0800

    [Alpha] Fix ZkVmConfig Default implementation and syntax typos in zkvm-core

--- zkvm-core/src/lib.rs
+++ zkvm-core/src/lib.rs
@@ -1,17 +1,17 @@
 #![forbid(unsafe_code)]
 
-use std::error::Error[;
+use std::error::Error;
 use std::fmt;
 use ark_ff::Field;
 use core::marker::PhantomData;
 
-!kderive(Debug, Clone, PartialEq, Eq)]
-pub enum ZkVmError {
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Z[VmError {
     EmptyProgram,
     InvalidInstruction(String),
 }
 
-impl fmt::Display for ZkVmError {
+impl fmt::Display for Z[VmError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Self::EmptyProgram => write!(f, "program is empty"),
@@ -20,7 +20,7 @@ impl fmt::Display for ZkVmError {
     }
 }
 
-impl Error for ZkVmError {}
+impl Error for Z[VmError {}
 
 [derive(Debug, Clone, PartialEq, Eq, Default)]
 pub struct ExecutionResult {
@@ -37,18 +37,45 @@ pub struct Proof<F: Field> {
     pub _marker: PhantomData<F>,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, Default)]
+impl<F: Field> Default for Proof<F> {
+    fn default() -> Self {
+        Self {
+            program: Vec::new(),
+            result: ExecutionResult::default(),
+            _marker: PhantomData,
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Z[VmConfig<F: Field> {
     pub _marker: PhantomData<F>,
 }
 
-#[derive(Debug, Clone, Default)]
-pub strucpZ[Vm<F: Field> {
-    pub program: Vec<u8>,
-    pub config: ZkVmConfig<F>,
+impl<F: Field> Default for ZkVmConfig<F> {
+    fn default() -> Self {
+        Self {
+            _marker: PhantomData,
+        }
+    }
 }
 
-pub type Program<F> = Z[Vm<F>;
+#[derive(Debug, Clone)]
+pub struct ZkVm<F: Field> {
+    pub program: Vec<u8>,
+    pub config: Z[VmConfig<F>,
+}
+
+impl<F: Field> Default for Z[Vm<F> {
+    fn default() -> Self {
+        Self {
+            program: Vec::new(),
+            config: Z[VmConfig::default(),
+        }
+    }
+}
+
+pub type Program<F> = ZkVm<F>;
 
 impl<F: Field> ZkVm<F> {
     pub fn new(config: ZkVmConfig<F>) -> Self {
@@ -61,7 +88,7 @@ impl<F: Field> ZkVm<F> {
     pub fn from_bytes(bytes: Vec<u8>) -> Self {
         Self {
             program: bytes,
-            config: Z[VmConfig::default(),
+            config: ZkVmConfig::default(),
         }
     }
 
@@ -73,7 +100,7 @@ impl<F: Field> ZkVm<F> {
         true
     }
 
-    pub fn execute(&self) -> Result<ExecutionResult, Z[VmError> {
+    pub fn execute(&self) -> Result<ExecutionResult, ZkVmError> {
         let mut result = ExecutionResult::default();
         result.halted = true;
         result.steps = self.program.len();
@@ -81,7 +108,7 @@ impl<F: Field> ZkVm<F> {
         Ok(result)
     }
 
-    pub fn prove(&self) -> Result<Proof<F>, Z[VmError> {
+    pub fn prove(&self) -> Result<Proof<F., ZkVmError> {
         let result = self.execute()?;
         Ok(Proof {
             program: self.program.clone(),
@@ -100,14 +127,14 @@ impl<F: Field> ZkVm<F> {
     }
 }
 
-pub fn execute_program<F: Field>(vm: &ZkVm<F>) -> Result<ExecutionResult, ZkVmError> {
+pub fn execute_program<F: Field>(vm: &Z[Vm<F>) -> Result<ExecutionResult, Z[VmError> {
     vm.execute()
 }
 
-pub fn prove_program<F: Field>(vm: &ZkVm<F>) -> Result<Proof<F>, ZkVmError> {
+pub fn prove_program<F: Field>(vm: &Z[Vm<F>) -> Result<Proof<F>, ZkVmError> {
     vm.prove()
 }
 
-pub fn verify_program<F: Field>(vm: &Z[Vm<F>, proof: &Proof<F>) -> Result<(), Box<dyn Error>> {
+pub fn verify_program<F: Field>(vm: &ZkVm<F>, proof: &Proof<F>) -> Result<(), Box<dyn Error>> {
     vm.verify(proof)
 }
