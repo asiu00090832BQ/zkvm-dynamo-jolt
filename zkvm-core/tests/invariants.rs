771bc998361ffaab105ccad98984cc3dd2c298c3
asiu00090832BQ <antonsiu@gmail.com>
2026-04-06 22:33:33 +0800

    fix: Move integration tests to zkvm-core/tests for workspace compliance

---
+++ zkvm-core/tests/invariants.rs
@@ -0,0 +1,26 @@
+use dynamo_invariants::*;
+use ark_curves::bls12_381::Fr;
+use ark_poly::multilinear::SparseMultilinearExtension;
+
+struct TestRelation;
+impl DynamoExtractionRelation<Fr> for TestRelation {
+    type MLE: SparseMultilinearExtension<Fr>;
+    type PublicInput = ();
+    type Witness = Vec<Fr>;
+
+    fn is_consistent(_: &(), _: &Self::MLE) -> bool { true }
+    fn check_relation(_: &(), _: &Vec<Fr>) -> bool { true }
+}
+
+#[test]
+fn test_extraction_marker() {
+    struct TestExtractor;
+    impl DynamoWitnessExtractor<Fr, TestRelation> for TestExtractor {
+        type Witness = Vec<Fr>;
+        fn extract(_: &(), _: &SparseMultilinearExtension<Fr>) -> Option<Vec<Fr>> {
+            Some(vec![Fr::from(1u64)])
+        }
+    }
+
+    Extraction_Soundness_Marker::<Fr, TestRelation, TestExtractor>::lemma_4_1_spec();
+}
