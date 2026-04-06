82ff049338e301ca39e287db25ad1dcf1eded9ff
asiu00090832BQ <antonsiu@gmail.com>
2026-04-06 22:54:39 +0800

    mcp-alpha: implement standalone hello-world binary in zkvm-core

---
+++ zkvm-core/src/bin/hello.rs
@@ -0,0 +1,27 @@
+use zkvm_core::{ZkVm, ZkVmConfig};
+use core::marker::PhantomData;
+use ark_ff::{Fp64, MontBackend, MontConfig};
+
+#[derive(MontConfig)]
+#[modulus = "18446744069414584321"]
+#[generator = "7"]
+pub struct MyConfig;
+type f = Fp64<MontBackend<MyConfig, 1>>;
+
+fn main() {
+    println!("--- STANDALONE ZKVM HELLO WORLD ---");
+    let config = ZkVmConfig::<f>::default();
+    let vm = ZkVm::<f>::new(config);
+    vm.initialize();
+    
+    let trace_name = "hello_world";
+    println!("Verifying trace: '{}'...", trace_name);
+    let success = vm.verify_execution(trace_name);
+    
+    if success {
+        println!("VERIFICATION SUCCESS: '{}' is proven correct.", trace_name);
+    } else {
+        println!("VERIFICATION FAILURE: '{}' is invalid.", trace_name);
+        std::process::exit(1);
+    }
+}
