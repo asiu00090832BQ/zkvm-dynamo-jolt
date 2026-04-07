cca66b7c6cd0f754beba2fb8d14e8856bf2bd3bb
asiu00090832BQ <antonsiu@gmail.com>
2026-04-06 21:29:27 -0400

    fix: Fix naming typo (ZcvmError to ZkvmError) in error.rs

--- zkvm-core/src/error.rs
+++ zkvm-core/src/error.rs
@@ -3,7 +3,7 @@ use crate::decoder::DecodeError;
 use crate::elf_loader::ElfLoaderError;
 
 #[derive(Debug)]
-pub enum ZcvmError {
+pub enum ZkvmError {
     Io(std::io::Error),
     InvalidElf(String),
     UnsupportedElf(String),
@@ -13,27 +13,27 @@ pub enum ZcvmError {
     ElfError(ElfLoaderError),
 }
 
-impl fmt::Display for ZcvmError {
+impl fmt::Display for ZkvmError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{self:?}")
+        write!(f, "{:?}", self)
     }
 }
 
-impl std::error::Error for ZcvmError {}
+impl std::error::Error for ZkvmError {}
 
-impl From<std::io::Error> for ZcvmError {
+impl From<std::io::Error> for ZkvmError {
     fn from(err: std::io::Error) -> Self {
         Self::Io(err)
     }
 }
 
-impl From<DecodeError> for ZcvmError {
+impl From<DecodeError> for ZkvmError {
     fn from(err: DecodeError) -> Self {
         Self::DecodeError(err)
     }
 }
 
-impl From<ElfLoaderError> for ZcvmError {
+impl From<ElfLoaderError> for ZkvmError {
     fn from(err: ElfLoaderError) -> Self {
         Self::ElfError(err)
     }
