bcdbe7fc82a992d891af6e6bdadb1fe4ff3e5bad
asiu00090832BQ <antonsiu@gmail.com>
2026-04-06 23:38:38 -0400

    fix: export missing modules and types in lib.rs

--- zkvm-core/src/lib.rs
+++ zkvm-core/src/lib.rs
@@ -1,6 +1,9 @@
 pub mod decoder;
 pub mod elf_loader;
 pub mod vm;
+pub mod frontend;
 
 pub use decoder::{decode, DecodeError, Decoder, Inst as Instruction};
 pub use elf_loader::{load_elf, ElfLoadError, LoadSegment, LoadedElf, SegmentFlags};
+pub use frontend::ElfProgram;
+pub use vm::{execute_program, prove_program, verify_program};
