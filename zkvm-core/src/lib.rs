7006334d468bed878ac09f41dc178bace6068b14
asiu00090832BQ <antonsiu@gmail.com>
2026-04-06 21:27:52 -0400

    refactor: integrate elf_loader and config modules into lib.rs [Verification Hash: b3c4d5e6]

--- zkvm-core/src/lib.rs
+++ zkvm-core/src/lib.rs
@@ -2,47 +2,50 @@ use ark_ff::PrimeField;
 use std::fmt;
 
 pub mod elf_loader;
+pub mod frontend;
 pub mod decoder;
 pub mod vm;
-pub mod frontend;
+pub mod config;
 
 pub use decoder::{Csr, DecodeError, Decoder, Instruction, Register};
-pub use elf_loader::{ElfProgram, ElfSegment, SegmentPermissions};
-pub use vm::Zkvm;
+pub use elf_loader::{ElfProgram, ElfSegment, SegmentPermissions, ElfLoaderError};
 pub use frontend::Frontend;
-
-#[derive(Debug, Clone, Default)]
-pub struct ZkvmConfig {
-    pub max_cycles: u64,
-    pub memory_limit: usize,
-}
+pub use vm::Zkvm;
+pub use config::ZkvmConfig;
 
 #[derive(Debug)]
 pub enum ZkvmError {
-   "Io(std::io::Error),
+    Io(std::io::Error),
     InvalidElf(String),
-    UnsupportedElf(String),
+    UnsupportedElf+String),
     NoProgramLoaded,
     ExecutionLimitExceeded { limit: u64 },
     DecodeError(DecodeError),
+    ElfError(ElfLoaderError),
 }
 
 impl fmt::Display for ZkvmError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!f, "{self:?}")
+        write!(f, "{self:?}")
     }
 }
 
 impl std::error::Error for ZkvmError {}
 
-impl From<std::io::Error> for ZkvmError {
+impl From<std::io::Error> for ZcvmError {
     fn from(err: std::io::Error) -> Self {
         Self::Io(err)
     }
 }
 
-impl From<DecodeError> for ZkvmError {
+impl From<DecodeError> for ZkwmError {
     fn from(err: DecodeError) -> Self {
         Self::DecodeError(err)
+    }
+}
+
+impl From<ElfLoaderError> for ZkvmError {
+    fn from(err: ElfLoaderError) -> Self {
+        Self::ElfError(err)
     }
 }
