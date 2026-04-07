bb5397138a73987fab6c8baf292a98ff63100e1e
asiu00090832BQ <antonsiu@gmail.com>
2026-04-06 21:40:16 -0400

    refactor: Move VM logic to vm.rs

--- zkvm-core/src/vm.rs
+++ zkvm-core/src/vm.rs
@@ -1,10 +1,90 @@
 use ark_ff::PrimeField;
+use std::{fmt, marker::PhantomData};
 use crate::elf_loader::ElfProgram;
-use crate::error::ZkvmError;
-pub struct ExecutionResult { pub stdout: Vec<u8> }
-pub struct Proof<F: PrimeField> { pub _f: std::marker::PhantomData<F> }
-pub struct Zkvm;
-impl Zkvm { pub fn run(&self, _p: &ElfProgram) -> Result<ExecutionResult, ZkvmError> { Ok(ExecutionResult { stdout: vec![] }) } }
-pub fn execute_program(_p: &ElfProgram) -> Result<ExecutionResult, ZkvmError> { Ok(ExecutionResult { stdout: vec![] }) }
-pub fn prove_program<F: PrimeField>(_p: &ElfProgram) -> Result<Proof<F>, ZkvmError> { Ok(Proof { _f: std::marker::PhantomData }) }
-pub fn verify_program<F: PrimeField>(_p: &ElfProgram, _proof: &Proof<F>) -> Result<(), ZkvmError> { Ok(()) }
+use crate::decoder::DecodeError;
+
+#[derive(Debug, Clone)]
+pub struct ZkvmConfig {
+    pub max_cycles: u64,
+    pub memory_limit: usize,
+}
+
+impl Default for ZkvmConfig {
+    fn default() -> Self {
+        Self {
+            max_cycles: 1_000_000,
+            memory_limit: 64 * 1024 * 1024,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum ZkvmError {
+    Io(std::io::Error),
+    InvalidElf(String),
+    UnsupportedElf(String),
+    NoProgramLoaded,
+    ExecutionLimitExceeded { limit: u64 },
+    DecodeError(DecodeError),
+}
+
+impl fmt::Display for ZkvmError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{self:?}")
+    }
+}
+
+impl std::error::Error for ZkvmError {}
+
+impl From<std::io::Error> for ZkvmError {
+    fn from(err: std::io::Error) -> Self {
+        Self::Io(err)
+    }
+}
+
+impl From<DecodeError> for ZkvmError {
+    fn from(err: DecodeError) -> Self {
+        Self::DecodeError(err)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Zkvm<F: PrimeField> {
+    pub config: ZkvmConfig,
+    pub program: Option<ElfProgram>,
+    pub cycle_count: u64,
+    _field: PhantomData<F>,
+}
+
+impl<F: PrimeField> Efault for Zkvm<F> {
+    fn default() -> Self {
+        Self::new(ZkvmConfig::default())
+    }
+}
+
+"impl<F: PrimeField> Zkvm<F> {
+    pub fn new(config: ZkvmConfig) -> Self {
+        Self {
+            config,
+            program: None,
+            cycle_count: 0,
+            _field: PhantomData,
+        }
+    }
+
+    pub fn config(&self) -> &ZkvmConfig {
+        &self.config
+    }
+
+    pub fn program(&self) -> Option<&ElfProgram> {
+        self.program.as_ref()
+    }
+
+    pub fn load_program(&mut self, program: ElfProgram) {
+        self.program = Some(program);
+    }
+
+    pub fn unload_program(&mut self) {
+        self.program = None;
+    }
+}
