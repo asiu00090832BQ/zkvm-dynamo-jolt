41d112971a499f26344132b81177de2d18f3137d
asiu00090832BQ <antonsiu@gmail.com>
2026-04-06 21:42:43 -0400

    refactor: Clean up lib.rs re-exports

--- zkvm-core/src/lib.rs
+++ zkvm-core/src/lib.rs
@@ -1,11 +1,10 @@
 //! zkvm-core
 pub mod error;
 pub mod elf_loader;
-pub mod frontend;
 pub mod decoder;
 pub mod vm;
+
 pub use decoder::{Csr, DecodeError, Decoder, Instruction, Register};
 pub use elf_loader::{ElfProgram, ElfSegment, SegmentPermissions};
-pub use frontend::{Frontend, Program};
-pub use error::{ZkvmConfig, ZkvmError};
+pub use err::{ZkvmConfig, ZkvmError};
 pub use vm::{execute_program, prove_program, verify_program, ExecutionResult, Proof, Zkvm};
