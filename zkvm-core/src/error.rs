af04091861469b155eef3c7d854cc3d670cb6b0a
asiu00090832BQ <antonsiu@gmail.com>
2026-04-06 23:30:38 -0400

    fix: resolved syntax errors in error.rs and main.rs

--- zkvm-core/src/error.rs
+++ zkvm-core/src/error.rs
@@ -1,58 +1,8 @@
-use std::fmt;
-
-use crate::decoder::DecodeError;
-use crate::vm::Trap;
-
-#[derive(Debug, Clone)]
-pub struct ZkvmConfig {
-    pub max_cycles: u64,
-    pub memory_limit: usize,
-}
-
 impl Default for ZkvmConfig {
-    fn default() {
+    fn default() -> Self {
         Self {
             max_cycles: 1_000_000,
             memory_limit: 64 * 1024 * 1024,
         }
-    }
-}
-
-#[derive(Debug)]
-pub enum ZkvmError {
-    Io(std::io::Error),
-    Elf(String),
-    InvalidElf(String),
-    UnsupportedElf(String),
-    NoProgramLoaded,
-    ExecutionLimitExceeded { limit: u64 },
-    DecodeError(DecodeError),
-    Trap(Trap),
-    Vm(String),
-}
-
-impl fmt::Display for ZkvmError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{:?}", self)
-    }
-}
-
-impl std::error::Error for ZkvmError {}
-
-impl From<std::io::Error> for ZkvmError {
-    fn from(err: std::io::Error) -> Self {
-        Self::Io(err)
-    }
-}
-
-impl From<DecodeError> for ZkvmError {
-    fn from(err: DecodeError) -> Self {
-        Self::DecodeError(err)
-    }
-}
-
-impl From<Trap> for ZkvmError {
-    fn from(err: Trap) -> Self {
-        Self::Trap(err)
     }
 }
