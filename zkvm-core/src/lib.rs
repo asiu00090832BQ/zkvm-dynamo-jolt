8ae259dd7d91c99fbaee9cdceecc1e91b0b3d54e
asiu00090832BQ <antonsiu@gmail.com>
2026-04-06 22:25:28 -0400

    Implementation: lib.rs exports for hardened modules (Phase 2)

--- zkvm-core/src/lib.rs
+++ zkvm-core/src/lib.rs
@@ -1,11 +1,4 @@
-pub mod error;
-pub mod frontend;
 pub mod decoder;
 pub mod elf_loader;
-pub mod vm;
-
-pub use error::{ZkvmConfig, ZkvmError};
-pub use decoder::{DecodeError, Instruction, decode};
-pub use frontend::ElfProgram;
-pub use elf_loader::{load_elf, LoadSegment, SegmentFlags, LoadedElf};
-pub use vm::{Vm, Memory, Trap, execute_program, prove_program, verify_program};
+pub use decoder::{decode, DecodeError, Decoder, Inst as Instruction};
+pub use elf_loader::{load_elf, ElfLoadError, LoadSegment, LoadedElf, SegmentFlags};
