9c3ca18ee267e207b985c4d0d96e5ae1e22bcf95
asiu00090832BQ <antonsiu@gmail.com>
2026-04-07 00:39:29 -0400

    fix(lib): high-integrity module exports and formatting

--- zkvm-core/src/lib.rs
+++ zkvm-core/src/lib.rs
@@ -1,72 +1,10 @@
-use ark_ff::PrimeField;
-use std::fmt;
-
-pub mod frontend;
 pub mod decoder;
 pub mod elf_loader;
+pub mod error;
+pub mod frontend;
 pub mod vm;
 
-pub use decoder::{DecodeError, Instruction, decode, DecoderConfig};
-pub use elf_loader::{LoadedProgram, Segment, load_elf, ElfLoaderError};
-pub use vm::{Vm, Memory, Trap};
-
-#[derive(Debug, Clone)]
-pub struct ZkvmConfig {
-    pub max_cycles: u64,
-    pub memory_limit: usize,
-    pub decoder_config: DecoderConfig,
-}
-
-impl Default for ZkvmConfig {
-    fn default() -> Self {
-        Self {
-            max_cycles: 1_000_000,
-            memory_limit: 64 * 1024 * 1024,
-            decoder_config: DecoderConfig::default(),
-        }
-    }
-}
-
-#[derive(Debug)]
-pub enum ZkvmError {
-    Io(std::io::Error),
-    ElfLoader(ElfLoaderError),
-    InvalidElf(String),
-    UnsupportedElf(String),
-    NoProgramLoaded,
-    ExecutionLimitExceeded { limit: u64 },
-    DecodeError(DecodeError),
-    Trap(Trap),
-}
-
-impl fmt::Display for ZkvmError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{self:?}")
-    }
-}
-
-impl std::error::Error for ZkvmError {}
-
-impl From<std::io::Error> for ZkvmError {
-    fn from(err: std::io::Error) -> Self {
-        Self::Io(err)
-    }
-}
-
-impl From<ElfLoaderError> for ZkvmError {
-    fn from(err: ElfLoaderError) -> Self {
-        Self::ElfLoader(err)
-    }
-}
-
-impl From<DecodeError> for ZkvmError {
-    fn from(err: DecodeError) -> Self {
-        Self::DecodeError(err)
-    }
-}
-
-impl From<Trap> for ZkvmError {
-    fn from(err: Trap) -> Self {
-        Self::Trap(err)
-    }
-}
+pub use crate::decoder::{decode, AluOp, BranchKind, Instruction, LoadWidth, MulOp, StoreWidth};
+pub use crate::elf_loader::{load_elf, LoadSegment, LoadedElf};
+pub use crate::error::{Result as ZkvmResult, ZkvmConfig, ZkvmError};
+pub use crate::vm::Zkvm;
