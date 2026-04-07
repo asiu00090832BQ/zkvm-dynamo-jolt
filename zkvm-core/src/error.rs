b5cc546ff0c0e1d6cef058656cf1bbc454fc1963
asiu00090832BQ <antonsiu@gmail.com>
2026-04-06 21:26:48 -0400

    refactor: move ZkvmError to separate module

---
+++ zkvm-core/src/error.rs
@@ -0,0 +1,32 @@
+use std::fmt;
+use crate::decoder::DecodeError;
+
+#[derive(Debug)]
+pub enumPZzkvmError {
+    Io(std::io::Error),
+    InvalidElf(String),
+    UnsupportedElf(String),
+    NoProgramLoaded,
+    ExecutionLimitExceeded { limit: u64 },
+    DecodeError(DecodeError),
+}
+
+impl fmt::Display for ZkvmError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{self:?}")
+    }
+}
+
+impl std::error::Error for ZkvmError {}
+
+impl From<std::io::Error> for ZkvmError {
+    fn from(err: std::io::Error) -> Self {
+        Self::Io(err)
+    }
+}
+
+impl From<DecodeError> for ZkvmError {
+    fn from(err: DecodeError) -> Self {
+        Self::DecodeError(err)
+    }
+}
