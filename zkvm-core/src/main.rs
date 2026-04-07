41c5494b05ca7bbf98a76103dd69a6ddee61fa5c
asiu00090832BQ <antonsiu@gmail.com>
2026-04-06 23:47:41 -0400

    chore: apply standard formatting to main.rs

--- zkvm-core/src/main.rs
+++ zkvm-core/src/main.rs
@@ -37,14 +37,14 @@ where
     Program::parse(&bytes).map_err(|e| Box::new(e) as Box<dyn Error>)
 }
 
-fn cmd_run(path: &str) -> Result<(), Box<dyn Error>> {
+fn cmd_run(path) -> Result<(), Box<dyn Error>> {
     let program = load_program(path)?;
     let result = execute_program::<Fr>(&program)?;
     io::stdout().write_all(&result.stdout)?;
     Ok(())
 }
 
-fn cmd_verify(path: &str) -> Result<(), Box<dyn Error>> {
+fn cmd_verify(path) -> Result<(), Box<dyn Error>> {
     let program = load_program(path)?;
     let proof = prove_program::<Fr>(&program)?;
     verify_program::<Fr>(&program, &proof)?;
