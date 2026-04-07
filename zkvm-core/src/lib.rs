2d5c57cddd2bb1afc6b1b7aad8ef306f1eecd6e9
asiu00090832BQ <antonsiu@gmail.com>
2026-04-07 00:55:40 -0400

    DNA.expression(Stabilizing lib.rs: Standardized module hierarchy and exports. Pipeline verified. Out.)

--- zkvm-core/src/lib.rs
+++ zkvm-core/src/lib.rs
@@ -6,28 +6,7 @@ pub mod error;
 pub mod frontend;
 pub mod vm;
 
-pub use decoder::{decode, DecodeError, DecoderConfig, Instruction};
-pub use elf_loader::{load_elf, ElfLoadError, LoadedElf, LoadSegment, SegmentFlags};
+pub use decoder::{decode, AluOp, BranchOp, DecodeError, Instruction, LoadWidth, MulOp, StoreWidth};
+pub use elf_loader::{load_elf_file, parse_elf, LoadedProgram, Segment};
 pub use error::{ZkvmConfig, ZkvmError};
-pub use vm::{Memory, Trap, Zkvm};
-pub use frontend::ElfProgram;
-
-#[derive(Debug, Clone)]
-pub struct ExecutionResult {
-    pub stdout: Vec<u8>,
-}
-
-#[derive(Debug, Clone)]
-pub struct ProgramProof;
-
-pub fn execute_program<F: PrimeField>(_program: &ElfProgram) -> Result<ExecutionResult, ZkvmError> {
-    Ok(ExecutionResult { stdout: Vec::new() })
-}
-
-pub fn prove_program<F: PrimeField>(_program: &ElfProgram) -> Result<ProgramProof, ZkvmError> {
-    Ok(ProgramProof)
-}
-
-pub fn verify_program<F: PrimeField>(_program: &ElfProgram, _proof: &ProgramProof) -> Result<(), ZkvmError> {
-    Ok(())
-}
+pub use vm::Vm;
