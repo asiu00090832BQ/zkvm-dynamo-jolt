cd476dcc26db10c8733df826dede4c5f1cc3aaf0
asiu00090832BQ <antonsiu@gmail.com>
2026-04-07 15:06:38 -0400

    style: apply rustfmt to zkvm-core/tests/zeroos_mem.rs

--- zkvm-core/tests/zeroos_mem.rs
+++ zkvm-core/tests/zeroos_mem.rs
@@ -1,22 +1,22 @@
-use zeroos_mem::*;
 use ark_bn254::Fr;
+use zeroos_mem::*;
 
 #[test]
 fn test_canonical_address_mapping() {
-    assert!(field_supports_64_bit_addresses::<Fr>());
-    
+    assert!(field_supports_64_bit_address_mapping::<Fr>());
+
     let addr: u64 = 0x1234567890ABCD1F;
     let field_val = canonical_addr_to_field::<Fr>(addr);
     let recovered_addr = field_to_canonical_addr(field_val);
-    
+
     assert_eq!(recovered_addr, Some(addr));
 }
 
 #[test]
-fn test_zero_address_mapping() {
+fn test_zero_address() {
     let addr: u64 = 0;
     let field_val = canonical_addr_to_field::<Fr>(addr);
     let recovered_addr = field_to_canonical_addr(field_val);
-    
+
     assert_eq!(recovered_addr, Some(0));
 }
