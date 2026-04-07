2f6f4422293941b7484ca54053fe273dec23d8bb
asiu00090832BQ <antonsiu@gmail.com>
2026-04-07 00:21:23 -0400

    fix: remediate incorrect 'public' keyword and align exports in lib.rs

--- zkvm-core/src/lib.rs
+++ zkvm-core/src/lib.rs
@@ -1,8 +1,8 @@
-public mod decoder;
-public mod elf_loader;
-public mod error;
-public mod frontend;
-public mod vm;
+pub mod decoder;
+pub mod elf_loader;
+pub mod error;
+pub mod frontend;
+pub mod vm;
 
 pub use decoder::{decode, DecodeError, DecoderConfig, Instruction};
 pub use elf_loader::{load_elf, ElfLoadError, LoadedElf, LoadSegment, SegmentFlags};
