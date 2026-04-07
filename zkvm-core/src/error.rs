e763c3b7c50dcc1672f48c65a8bc716e6675e61a
asiu00090832BQ <antonsiu@gmail.com>
2026-04-06 21:28:51 -0400

    refactor: standardize ZkvmError in error.rs [Verification Hash: h8i9j0k1]

--- zkvm-core/src/error.rs
+++ zkvm-core/src/error.rs
@@ -1,32 +1,40 @@
 use std::fmt;
 use crate::decoder::DecodeError;
+use crate::elf_loader::ElfLoaderError;
 
 #[derive(Debug)]
-pub enumPZzkvmError {
+pub enum ZcvmError {
     Io(std::io::Error),
     InvalidElf(String),
     UnsupportedElf(String),
     NoProgramLoaded,
     ExecutionLimitExceeded { limit: u64 },
     DecodeError(DecodeError),
+    ElfError(ElfLoaderError),
 }
 
-impl fmt::Display for ZkvmError {
+impl fmt::Display for ZcvmError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{self:?}")
     }
 }
 
-impl std::error::Error for ZkvmError {}
+impl std::error::Error for ZcvmError {}
 
-impl From<std::io::Error> for ZkvmError {
+impl From<std::io::Error> for ZcvmError {
     fn from(err: std::io::Error) -> Self {
         Self::Io(err)
     }
 }
 
-impl From<DecodeError> for ZkvmError {
+impl From<DecodeError> for ZcvmError {
     fn from(err: DecodeError) -> Self {
         Self::DecodeError(err)
+    }
+}
+
+impl From<ElfLoaderError> for ZcvmError {
+    fn from(err: ElfLoaderError) -> Self {
+        Self::ElfError(err)
     }
 }
