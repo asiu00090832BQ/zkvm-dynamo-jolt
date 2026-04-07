f60801b365f5ccd9daee27b8beb65eb5c1f35f5b
asiu00090832BQ <antonsiu@gmail.com>
2026-04-07 00:32:17 -0400

    fix: align lib.rs with Phase 2 implementation specs

--- zkvm-core/src/lib.rs
+++ zkvm-core/src/lib.rs
@@ -1,33 +1,72 @@
 use ark_ff::PrimeField;
+use std::fmt;
 
+pub mod frontend;
 pub mod decoder;
 pub mod elf_loader;
-pub mod error;
-pub mod frontend;
 pub mod vm;
 
-pub use decoder::{decode, DecodeError, DecoderConfig, Instruction};
-pub use elf_loader::{load_elf, ElfLoadError, LoadedElf, LoadSegment, SegmentFlags};
-pub use error::{ZkvmConfig, ZkvmError};
-pub use vm::{Memory, Trap, Zkvm};
-pub use frontend::ElfProgram;
+pub use decoder::{DecodeError, Instruction, decode, DecoderConfig};
+pub use elf_loader::{LoadedProgram, Segment, load_elf, ElfLoaderError};
+pub use vm::{Vm, Memory, Trap};
 
-[derive(Debug, Clone)]
-pub struct ExecutionResult {
-    pub stdout: Vec<u8>,
+#[derive(Debug, Clone)]
+pub struct ZkvmConfig {
+    pub max_cycles: u64,
+    pub memory_limit: usize,
+    pub decoder_config: DecoderConfig,
 }
 
-[derive(Debug, Clone)]
-pub struct ProgramProof;
-
-pub fn execute_program<F: PrimeField>(_program: &ElfProgram) -> Result<ExecutionResult, ZkvmError> {
-    Ok(ExecutionResult { stdout: Vec::new() })
+impl Default for ZkvmConfig {
+    fn default() -> Self {
+        Self {
+            max_cycles: 1_000_000,
+            memory_limit: 64 * 1024 * 1024,
+            decoder_config: DecoderConfig::default(),
+        }
+    }
 }
 
-pub fn prove_program<F: PrimeField>(_program: &ElfProgram) -> Result<ProgramProof, ZkvmError> {
-    Ok(ProgramProof)
+#[derive(Debug)]
+pub enum ZkvmError {
+    Io(std::io::Error),
+    ElfLoader(ElfLoaderError),
+    InvalidElf(String),
+    UnsupportedElf(String),
+    NoProgramLoaded,
+    ExecutionLimitExceeded { limit: u64 },
+    DecodeError(DecodeError),
+    Trap(Trap),
 }
 
-pub fn verify_program<F: PrimeField>(_program: &ElfProgram, _proof: &ProgramProof) -> Result<(), ZkvmError> {
-    Ok(())
+impl fmt::Display for ZkvmError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{self:?}")
+    }
+}
+
+impl std::error::Error for ZkvmError {}
+
+impl From<std::io::Error> for ZkvmError {
+    fn from(err: std::io::Error) -> Self {
+        Self::Io(err)
+    }
+}
+
+impl From<ElfLoaderError> for ZkvmError {
+    fn from(err: ElfLoaderError) -> Self {
+        Self::ElfLoader(err)
+    }
+}
+
+impl From<DecodeError> for ZkvmError {
+    fn from(err: DecodeError) -> Self {
+        Self::DecodeError(err)
+    }
+}
+
+impl From<Trap> for ZkvmError {
+    fn from(err: Trap) -> Self {
+        Self::Trap(err)
+    }
 }
