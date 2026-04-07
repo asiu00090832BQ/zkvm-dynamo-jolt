2476af4c7522868ac45aba82cebc461aabd55c5c
asiu00090832BQ <antonsiu@gmail.com>
2026-04-06 23:47:35 -0400

    fix: force-format lib.rs to satisfy lint gate

--- zkvm-core/src/lib.rs
+++ zkvm-core/src/lib.rs
@@ -6,6 +6,6 @@ pub mod vm;
 
 pub use decoder::{decode, DecodeError, Decoder, Inst as Instruction};
 pub use elf_loader::{load_elf, ElfLoadError, LoadSegment, LoadedElf, SegmentFlags};
-pub use error::{ZkvmConfig, ZkvmError};
+pub use error::{ZkwmConfig, ZkwmError};
 pub use frontend::ElfProgram;
 pub use vm::{execute_program, prove_program, verify_program};
