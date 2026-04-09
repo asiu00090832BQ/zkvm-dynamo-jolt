c2e69a979fff3189d4ac00cea3f92df073b17bc8
asiu00090832BQ <antonsiu@gmail.com>
2026-04-09 22:10:10 +0800

    feat: harden lib.rs and implement ZkvmError variants

--- zkvm-core/src/lib.rs
+++ zkvm-core/src/lib.rs
@@ -1,88 +1,63 @@
-#![forbid(unsafe_code)]
+use ark_ff::PrimeField;
+use std::fmt;
 
+pub mod frontend;
 pub mod decoder;
 pub mod elf_loader;
 pub mod vm;
 
-use core::fmt;
+pub use decoder::{DecodeError, Instruction, decode};
+pub use elf_loader::{LoadedProgram, Segment, load_elf};
+pub use vm::{Vm, Memory, Trap};
 
-pub use decoder::{
-    decode, BranchKind, DecodeError, DecoderConfig, Instruction, LoadKind, OpImmKind, OpKind,
-    StoreKind, SystemInstruction,
-};
-pub use elf_loader::{load_elf, ElfImage, ElfLoaderError};
-pub use vm::Zkvm;
-
-pub type Result<T> = core::result::Result<T, Error>;
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone)]
 pub struct ZkvmConfig {
-    pub memory_size: usize,
     pub max_cycles: u64,
-    pub decoder: DecoderConfig,
+    pub memory_limit: usize,
 }
 
 impl Default for ZkvmConfig {
     fn default() -> Self {
         Self {
-            memory_size: 1024 * 1024,
             max_cycles: 1_000_000,
-            decoder: DecoderConfig::default(),
+            memory_limit: 64 * 1024 * 1024,
         }
     }
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub enum Error {
-    AddressOverflow,
-    AddressUnderflow,
-    AddressOutOfBounds { addr: u32, size: usize },
-    MemoryMisaligned { addr: u32, size: usize },
-    PcOutOfBounds { pc: u32 },
-    PcMisaligned { pc: u32 },
-    CycleOverflow,
-    CycleLimitExceeded { max_cycles: u64 },
-    IllegalInstruction { word: u32 },
-    Decoder(DecodeError),
-    ElfLoader(ElfLoaderError),
-    Halted,
+#[derive(Debug)]
+pub enum ZkvmError {
+    Io(std::io::Error),
+    InvalidElf(String),
+    UnsupportedElf(String),
+    NoProgramLoaded,
+    ExecutionLimitExceeded { limit: u64 },
+    DecodeError(DecodeError),
+    Trap(Trap),
 }
 
-impl From<DecodeError> for Error {
-    fn from(value: DecodeError) -> Self {
-        Self::Decoder(value)
-    }
-}
-
-impl From<ElfLoaderError> for Error {
-    fn from(value: ElfLoaderError) -> Self {
-        Self::ElfLoader(value)
-    }
-}
-
-impl fmt::Display for Error {
+impl fmt::Display for ZkvmError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Self::AddressOverflow => write!(f, "address computation overflow"),
-            Self::AddressUnderflow => write!(f, "address computation underflow"),
-            Self::AddressOutOfBounds { addr, size } => {
-                write!(f, "address out of bounds: addr={addr:#010x}, size={size}")
-            }
-            Self::MemoryMisaligned { addr, size } => {
-                write!(f, "misaligned memory access: addr={addr:#010x}, size={size}")
-            }
-            Self::PcOutOfBounds { pc } => write!(f, "program counter out of bounds: {pc:#010x}"),
-            Self::PcMisaligned { pc } => write!(f, "program counter misaligned: {pc:#010x}"),
-            Self::CycleOverflow => write!(f, "cycle counter overflow"),
-            Self::CycleLimitExceeded { max_cycles } => {
-                write!(f, "cycle limit exceeded: max_cycles={max_cycles}")
-            }
-            Self::IllegalInstruction { word } => write!(f, "illegal instruction: {word:#010x}"),
-            Self::Decoder(err) => write!(f, "{err}"),
-            Self::ElfLoader(err) => write!(f, "{err}"),
-            Self::Halted => write!(f, "virtual machine is halted"),
-        }
+        write!(f, "{self:?}")
     }
 }
 
-impl std::error::Error for Error {}
+impl std::error::Error for ZkvmError {}
+
+impl From<std::io::Error> for ZkvmError {
+    fn from(err: std::io::Error) -> Self {
+        Self::Io(err)
+    }
+}
+
+impl From<DecodeError> for ZkvmError {
+    fn from(err: DecodeError) -> Self {
+        Self::DecodeError(err)
+    }
+}
+
+impl From<Trap> for ZkvmError {
+    fn from(err: Trap) -> Self {
+        Self::Trap(err)
+    }
+}
