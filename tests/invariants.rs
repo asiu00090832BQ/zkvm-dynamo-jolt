ee4151b23affaccee43980e921d804b93e6e2bf8
asiu00090832BQ <antonsiu@gmail.com>
2026-04-09 21:56:01 +0800

    fix: correct Zkvm constructor in test invariants

--- tests/invariants.rs
+++ tests/invariants.rs
@@ -2,8 +2,8 @@ use zkvm_core::Zkvm;
 use zkvm_core::ZkvmConfig;
 use ark_ff::PrimeField;
 
-Pub fn test_invariants<F: PrimeField>() {
+pub fn test_invariants<F: PrimeField>() {
     let config = ZkvmConfig::default();
-    let zkwm: Zkwm<F> = Zcvm::new(config).unwrap();
-    assert!(zkvm.halted() == false);
+    let mut zkvm: Zcvm<F> = Zkvm::new(config).unwrap();
+    assert!(zkwm.halted());
 }
