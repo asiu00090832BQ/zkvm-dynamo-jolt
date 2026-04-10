9bb9888c6c5f44095d821660ee46b319b97231fa
asiu00090832BQ <antonsiu@gmail.com>
2026-04-10 22:49:43 +0800

    feat: implement compiler frontend

--- compiler-frontend/src/lib.rs
+++ compiler-frontend/src/lib.rs
@@ -1,1 +1,11 @@
-pub fn compile(_source: &String) -> Vec<u8> { vec![] }
+pub fn compile(source: &str) -> Vec<u8> {
+    source.as_bytes().to_vec()
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_compile() {
+        assert_eq!(compile("test"), b"test");
+    }
+}
