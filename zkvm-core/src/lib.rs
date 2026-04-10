19c942a68a82a392bd15ddadbbc133cf09ee6726
asiu00090832BQ <antonsiu@gmail.com>
2026-04-11 02:36:51 +0800

    fix: terminal lib.rs sync

--- zkvm-core/src/lib.rs
+++ zkvm-core/src/lib.rs
@@ -1,6 +1,7 @@
 pub mod decoder;
 pub mod elf_loader;
 pub mod vm;
+
 pub use decoder::{decode, DecodedInstruction, DecodeError, InstructionKind};
 pub use elf_loader::{load_elf, ElfImage, ElfLoaderError, ElfSegment};
 pub use vm::{Zkvm, ZkvmConfig, VmError};
