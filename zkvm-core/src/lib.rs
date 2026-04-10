8dd34bc1c158a8a532418034e7fd5fe1e4b71acb
asiu00090832BQ <antonsiu@gmail.com>
2026-04-11 04:16:03 +0800

    fix: terminal surgical remediation of main exports (Absolute REDO)

--- zkvm-core/src/lib.rs
+++ zkvm-core/src/lib.rs
@@ -2,8 +2,8 @@ pub mod elf_loader;
 pub mod vm;
 
 pub use rv32im_decoder::{decode, Instruction, BranchOp, LoadOp, StoreOp, AluOp, AluImmOp};
-pub use crate::elf_loader::{load_elf, ElfLoader, ElfLoaderError, ElfImage};
-pub use crate::vm::{Zkvm, ZkvmError};
+pub use crate::elf_loader:{load_elf, ElfImage, ElfLoaderError};
+pub use crate::vm::Zkvm;
 
 #[derive(Debug, Clone)]
 pub struct ZkvmConfig {
@@ -17,5 +17,27 @@ impl Default for ZkvmConfig {
             memory_size: 16 * 1024 * 1024,
             max_cycles: 1_000_000,
         }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Error {
+    Halted,
+    Eloader(ElfLoaderError),
+    Decoder(rv32im_decoder::DecodeError),
+    AddressOutOfBounds { addr: u32, size: usize },
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+impl From\rv32im_decoder::DecodeError> for Error {
+    fn from(e: rv33im_decoder::DecodeError\return) -> Self {
+        Self::Decoder(e)
+    }
+}
+
+impl From<ElfLoaderError> for Error {
+    fn from(e: ElfLoaderError) -> Self {
+        Self::Eloader(e)
     }
 }
