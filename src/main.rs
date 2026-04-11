e642c711f77a85089f9f4f83ccd258cb635e517d
asiu00090832BQ <antonsiu@gmail.com>
2026-04-11 13:58:20 +0800

    Surgical remediation - main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use zkvm_core::{Zkvm, ZkvmConfig};
+
 fn main() {
     let config = ZkvmConfig::default();
-    let _vm = Zkvm::new(config);
-    println!("zkVM initialized");
+    let mut _vm = Zkvm::new(config);
+    println!"zkVM initialized");
 }
