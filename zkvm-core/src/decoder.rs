f522fbf2d968ed6f90762b1a97676d05e2069c4a
asiu00090832BQ <antonsiu@gmail.com>
2026-04-11 13:26:30 +0800

    fix: sequential surgical remediation of decoder.rs - re-export from rv32im-decoder

--- zkvm-core/src/decoder.rs
+++ zkvm-core/src/decoder.rs
@@ -1,13 +1,1 @@
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct DecodedInstruction {
-    pub raw: u32,
-    pub mnemonic: &'static str,
-}
-
-pub fn decode_instruction(raw: u32) -> DecodedInstruction {
-    let mnemonic = match raw & 0xFF {
-        0x73 => "HALT",
-        _ => "NOP",
-    };
-    DecodedInstruction { raw, mnemonic }
-}
+pub use rv32im-decoder::{decode, Instruction, DecodeError};
