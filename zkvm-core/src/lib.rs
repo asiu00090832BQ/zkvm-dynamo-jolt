785b7775064970023bc2afe7f7f3cf622966ba80
asiu00090832BQ <antonsiu@gmail.com>
2026-04-11 14:27:54 +0800

    Update zkvm-core/src/lib.rs

--- zkvm-core/src/lib.rs
+++ zkvm-core/src/lib.rs
@@ -1,6 +1,6 @@
-pub mod decoder;
-pub mod elf_loader;
 pub mod vm;
 
-pub use decoder::{decode_instruction, DecodeError, Instruction};
+pub use rv32im_decoder::{
+    decode_instruction, BranchOp, CsrOp, DecodeError, Instruction, LoadOp, OpImmOp, OpOp, StoreOp,
+};
 pub use vm::{RunStats, StepOutcome, VmError, Zkvm, ZkvmConfig};
