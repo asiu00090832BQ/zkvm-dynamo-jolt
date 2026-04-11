572eb471b5ba5899f870f17f59d7cf53c33c54dc
asiu00090832BQ <antonsiu@gmail.com>
2026-04-11 13:59:05 +0800

    fix: terminal surgical remediation of main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
+use ark_bn254::Fr;
 use zkvm_core::{Zkvm, ZkvmConfig};
+
 fn main() {
-    let config = ZkvmConfig::default();
-    let _vm = Zkvm::new(config);
-    println!("zkVM initialized");
+    let config = ZkvmConfig {
+        memory_size: 1 << 20,
+    };
+
+    let mut vm: Zkvm<Fr> = Zkvm::new(config);
+
+    println!(
+        "Initialized zkvm with memory size {} bytes",
+        vm.config.memory_size
+    );
+
+    let _ = vm;
 }
