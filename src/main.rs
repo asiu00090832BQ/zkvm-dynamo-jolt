0775d39d22fe710b2784b0129516043c86dd3dab
asiu00090832BQ <antonsiu@gmail.com>
2026-04-11 14:19:17 +0800

    chore: restore stable src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,8 @@ use ark_bn254::Fr;
 use zkvm_core::{Zkvm, ZkvmConfig};
 
 fn main() {
-    let config = ZkvmConfig {
-        memory_size: 1 << 20,
-    };
+    let config = ZkvmConfig::default();
+    let vm = Zkvm::<Fr>::new(config);
 
-    let mut vm: Zkvm<Fr> = Zkvm::new(config);
-
-    println!(
-        "Initialized zkvm with memory size {} bytes",
-        vm.config.memory_size
-    );
-
-    let _ = vm;
+    println!("memory size: {} bytes", vm.memory().len());
 }
