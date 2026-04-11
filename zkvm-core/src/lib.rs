26aaee3a804732ab24c4e3c4895b079aeb05c4a0
asiu00090832BQ <antonsiu@gmail.com>
2026-04-11 13:26:26 +0800

    fix: sequential surgical remediation of lib.rs - correct exports

--- zkvm-core/src/lib.rs
+++ zkvm-core/src/lib.rs
@@ -1,7 +1,6 @@
+pub mod vm;
 pub mod decoder;
 pub mod elf_loader;
-pub mod vm;
 
-pub use crate::decoder::{decode_instruction, DecodedInstruction};
-pub use crate::elf_loader::*;
-pub use crate::vm::{Outcome, RunStats, Zkvm, ZkvmConfig};
+pub use vm::{Zkvm, ZkvmConfig, VmError, RunStats, VmOutcome};
+pub use decoder::{decode, Instruction, DecodeError};
