ce119fe5738fcdd533d3a907e206eaecac5df0a4
asiu00090832BQ <antonsiu@gmail.com>
2026-04-11 13:52:00 +0800

    fix: terminal surgical remediation of lib.rs - synchronize API

--- zkvm-core/src/lib.rs
+++ zkvm-core/src/lib.rs
@@ -1,6 +1,6 @@
+pub mod vm;
 pub mod decoder;
 pub mod elf_loader;
-pub mod vm;
 
-pub use decoder::{decode, Instruction};
-pub use vm::{Zkvm, ZkvmConfig, VmError, RunStats, Outcome};
+pub use crate::vm::{Zkvm, ZkvmConfig, VmError, RunStats, VmOutcome};
+pub use crate::decoder::{decode, Instruction};
