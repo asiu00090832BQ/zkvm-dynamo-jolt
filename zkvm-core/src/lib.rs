9f243e9eaa3b4eac874afdc6a0ef480f8eef9ad5
asiu00090832BQ <antonsiu@gmail.com>
2026-04-11 13:45:02 +0800

    fix: surgical remediation of lib.rs - align exports

--- zkvm-core/src/lib.rs
+++ zkvm-core/src/lib.rs
@@ -1,5 +1,6 @@
 pub mod decoder;
 pub mod elf_loader;
 pub mod vm;
-pub use decoder::{decode, Instruction, DecodeError};
-pub use vm::{Zkvm, ZkvmConfig, VmError, RunStats, StepOutcome};
+
+pub use decoder::{decode, Instruction};
+pub use vm::{Zkvm, ZkvmConfig, VmError, RunStats, Outcome};
