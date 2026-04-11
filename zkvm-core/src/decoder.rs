3930294188f7c9e3da5875c99a771bd2469de182
asiu00090832BQ <antonsiu@gmail.com>
2026-04-11 13:33:21 +0800

    Surgical stabilization of decoder.rs

--- zkvm-core/src/decoder.rs
+++ zkvm-core/src/decoder.rs
@@ -1,1 +1,18 @@
-pub use rv32im-decoder::{decode, Instruction, DecodeError};
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Instruction {
+    Ecall,
+    Invalid(u32),
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum DecodeError {
+    Invalid(u32),
+}
+
+pub fn decode(word: u32) -> Result<Instruction, DecodeError> {
+    if word == 0x73 {
+        Ok(Instruction::Ecall)
+    } else {
+        Ok(Instruction::Invalid(word))
+    }
+}
