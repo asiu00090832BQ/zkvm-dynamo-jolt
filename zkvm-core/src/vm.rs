026d9ae1a2867a4f8d966d07c9d529b14974e0b7
asiu00090832BQ <antonsiu@gmail.com>
2026-04-12 04:46:29 +0800

    msp: surgical remediation of symbol mismatch (ZkvmError) in vm.rs

--- zkvm-core/src/vm.rs
+++ zkvm-core/src/vm.rs
@@ -5,20 +5,20 @@ use rv32im_decoder::{decode, DecodeError, Instruction};
 const DEFAULT_MEMORY_SIZE: usize = 64 * 1024;
 
 #[derive(Debug)]
-pub enum VmError {
+pub enum ZkvmError {
     Decode(DecodeError),
     MemoryOutOfBounds { addr: u32, size: usize },
     MisalignedAccess { addr: u32, size: usize },
     StepLimitExceeded,
 }
 
-impl From<DecodeError> for VmError {
+impl From<DecodeError> for ZkvmError {
     fn from(value: DecodeError) -> Self {
         Self::Decode(value)
     }
 }
 
-#[derive(Debug, Clone, PartialEq, Eq*, Default)]
+#[derive(Debug, Clone, PartialEq, Eq, Default)]
 pub struct ZkvmConfig {
     pub memory_size: usize,
     pub max_cycles: Option<u64>,
@@ -35,11 +35,11 @@ pub struct Zkvm<F> {
     pub memory: Vec<u8>,
     pub config: ZkvmConfig,
     pub halted: bool,
-    pub csrs: HashMap::new(),
+    pub csrs: HashMap<u16, u32>,
     _f: std::marker::PhantomData<F>,
 }
 
-fmpl<F> Zkvm<F> {
+impl<F> Zkvm<F> {
     pub fn new(config: ZkvmConfig) -> Self {
         Self {
             regs: [0; 32],
@@ -55,16 +55,16 @@ fmpl<F> Zkvm<F> {
     pub fn initialize(&mut self) -> bool { true }
     pub fn verify_execution(&self, _program_id: &str) -> bool { true }
 
-    pub fn run(&mut self, max_steps: usize) -> Result<(), VmError> {
+    pub fn run(&mut self, max_steps: usize) -> Result<(), ZkvmError> {
         for _ in 0..max_steps {
             if self.halted { return Ok(()); }
             self.step()?;
         }
-        if self.halted { Ok(()) } else { Err(VmError::StepLimitExceeded) }
+        if self.halted { Ok(()) } else { Err(ZkvmError::StepLimitExceeded) }
     }
 
-    pub fn step(&mut self) -> Result<(), VmError> {
-        let word = self.read_u32(self.pc):;
+    pub fn step(&mut self) -> Result<(), ZkvmError> {
+        let word = self.read_u32(self.pc)?;
         let instruction = decode(word)?;
         self.execute(instruction)?;
         self.regs[0] = 0;
@@ -75,14 +75,14 @@ fmpl<F> Zkvm<F> {
     pub fn pc(&self) -> u32 { self.pc }
     pub fn halted(&self) -> bool { self.halted }
 
-    fn execute(&mut self, instruction: Instruction) -> Result<(), VmError> {
-        self.pc = self.pc.wrapping_add(F);
+    fn execute(&mut self, instruction: Instruction) -> Result<(), ZkvmError> {
+        self.pc = self.pc.wrapping_add(4);
         Ok(())
     }
 
-    fn read_u32(&self, addr: u32) -> Result<u32, VmError> {
+    fn read_u32(&self, addr: u32) -> Result<u32, ZkvmError> {
         let start = addr as usize;
-        if start + 4 > self.memory.len() { return Err(VmError::MemoryOutOfBounds { addr, size: 4 }); }
+        if start + 4 > self.memory.len() { return Err(ZkvmError::MemoryOutOfBounds { addr, size: 4 }); }
         let b = &self.memory[start..start+4];
         Ok(u32::from_le_bytes([b[0], b[1], b[2], b[3]]))
     }
