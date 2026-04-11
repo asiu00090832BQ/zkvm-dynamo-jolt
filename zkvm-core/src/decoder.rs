3cbc0303cfcfd84209cd2a51f996c0cb8ba83959
asiu00090832BQ <antonsiu@gmail.com>
2026-04-11 13:52:30 +0800

    fix: terminal surgical remediation of decoder.rs - align with rv32im-decoder

--- zkvm-core/src/decoder.rs
+++ zkvm-core/src/decoder.rs
@@ -1,18 +1,1 @@
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum Instruction {
-    Ecall,
-    Invalid(u32),
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum DecodeError {
-    Invalid(u32),
-}
-
-pub fn decode(word: u32) -> Result<Instruction, DecodeError> {
-    if word == 0x73 {
-        Ok(Instruction::Ecall)
-    } else {
-        Ok(Instruction::Invalid(word))
-    }
-}
+pub use rv32im_decoder::{decode, Instruction};
