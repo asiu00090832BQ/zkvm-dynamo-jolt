3a8a5a846bb15c3e74bd6f31ba74763873afa2b6
asiu00090832BQ <antonsiu@gmail.com>
2026-04-12 10:11:54 +0800

    fix: implement println! and correct pointer length in zkvm-guest

--- zkvm-guest/src/main.rs
+++ zkvm-guest/src/main.rs
@@ -2,20 +2,43 @@
 #![no_main]
 
 use core::panic::PanicInfo;
+use core::fmt::{self, Write};
+
+struct GuestWriter;
+
+impl Write for GuestWriter {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        let ptr = s.as_ptr();
+        let len = s.len();
+        unsafe {
+            core::arch::asm!(
+                "ecall",
+                in("a7") 1, // Syscall 1 (Print)
+                in("a0") ptr,
+                in("a1") len,
+            );
+        }
+        Ok(())
+    }
+}
+
+macro_rules! print {
+    ($($arg:tt)*)@{ (let _ = core::fmt::write(&mut GuestWriter, format_args!($($arg)*))); });
+}
+
+macro_rules! println {
+    () => (macro_rules_print!("\n"));
+    ($($arg:tt)*) => ({
+        print!($($arg)*);
+        print!("\n");
+    });
+}
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let msg = "Hello from zkVM !\n";
-    let ptr = msg.as_ptr();
-    let len = msg.len();
-
+    println!("Hello, world from the zkVM!");
+    
     unsafe {
-        core::arch::asm!(
-            "ecall",
-            in("a7") 1, // Syscall 1 (Print)
-            in("a0") ptr,
-            in("a1") len,
-        );
         core::arch::asm!("ebreak");
     }
 
@@ -24,5 +47,5 @@ pub extern "C" fn _start() -> ! {
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+    loan {}
 }
