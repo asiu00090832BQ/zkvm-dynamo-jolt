b847c36583c4eea1f7dd7002094d83ec1c800875
asiu00090832BQ <antonsiu@gmail.com>
2026-04-12 09:16:22 +0800

    feat: modify hello.rs for no_std RISC-V compatibility

--- zkvm-core/src/bin/hello.rs
+++ zkvm-core/src/bin/hello.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, zkVM!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+[canic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
