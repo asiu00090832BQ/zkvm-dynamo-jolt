93e8189835b57084b228c2bdb3e2a6bed9a8ca07
asiu00090832BQ <antonsiu@gmail.com>
2026-04-12 09:27:33 +0800

    fix: resolve duplicate panic_handler and zkvm bin naming

---
+++ zkvm-core/src/bin/hello.rs
@@ -0,0 +1,25 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+static mut RESULT: u32 = 0;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let mut sum: u32 = 0;
+    for i in 0..=100 {
+        sum = sum.wrapping_add(i);
+    }
+    unsafe {
+        RESULT = sum;
+    }
+    loop {}
+}
+
+#[cfg(target_arch = "riscv32")]
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
