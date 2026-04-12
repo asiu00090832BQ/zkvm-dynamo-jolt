bdb5d1996483b11f8f0a4f14af4f9fca02964855
asiu00090832BQ <antonsiu@gmail.com>
2026-04-12 09:36:54 +0800

    feat: hello world guest with Ecall print

--- guest/src/main.rs
+++ guest/src/main.rs
@@ -4,18 +4,23 @@
 use core::panic::PanicInfo;
 
 #[no_mangle]
-static mut RESULT: u32 = 0;
-
-#[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let mut sum: u32 = 0;
-    for i in 0..=100 {
-        sum = sum.wrapping_add(i);
-    }
-    unsafe {
-        RESULT = sum;
-    }
+    let msg = "Hello World!\n";
+    print(msg);
     loop {}
+}
+
+fn print(s: &str) {
+    let ptr = s.as_ptr() as u32;
+    let len = s.len() as u32;
+    unsafe {
+        core::arch::asm!(
+            "ecall",
+            in("a7") 1,
+            in("a0") ptr,
+            in("a1") len,
+        );
+    }
 }
 
 #[panic_handler]
