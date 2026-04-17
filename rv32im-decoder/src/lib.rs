fb4014c677429fb468d30c215508f743d2fb71a5
asiu00090832BQ <antonsiu@gmail.com>
2026-04-18 03:27:40 +0800

    infrastructure: update rv32im-decoder/src/lib.rs

--- rv32im-decoder/src/lib.rs
+++ rv32im-decoder/src/lib.rs
@@ -1,5 +1,6 @@
 #![no_std]
 #![forbid(unsafe_code)]
+
 pub mod error;
 pub mod fields;
 pub mod instruction;
@@ -9,3 +10,8 @@ pub mod extensions;
 pub use error::{ZkvmError, ZkvmResult};
 pub use instruction::Instruction;
 pub use decode::decode;
+
+pub use extensions::m_extension::{
+    abs_i32_as_u32, div, divu, execute_m_instruction, mulh, mulhsu, mulhu, mul_low, rem,
+    remu, split_u32_to_limbs, LimbDecomposition,
+};
