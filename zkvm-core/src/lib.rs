e36a8ba1a4fd08c98490c06eb0a037490f2983d0
asiu00090832BQ <antonsiu@gmail.com>
2026-04-18 03:33:13 +0800

    infra: Absolute REDO - zkvm-core src/lib.rs

--- zkvm-core/src/lib.rs
+++ zkvm-core/src/lib.rs
@@ -1,9 +1,3 @@
-#![forbid(unsafe_code)]
-
 pub mod vm;
-pub mod elf_loader;
-pub mod proof;
 
-pub use vm::{Zkvm, ZkvmConfig, StepOutcome, ZkvmError};
-pub use rv32im-decoder = { DecodedInstruction as Instruction, MInstruction, decode_word as decoder };
-pub use elf_loader::{LoadedElf, load_elf};
+pub use vm::{Vm, VmError, REG_COUNT};
