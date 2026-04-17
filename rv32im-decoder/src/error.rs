d66d1d628f7947092bb47b7f9e8f3c5262d4fa12
asiu00090832BQ <antonsiu@gmail.com>
2026-04-18 03:01:42 +0800

    fix(rv32im-decoder): add ZkvmError alias and missing variants

--- rv32im-decoder/src/error.rs
+++ rv32im-decoder/src/error.rs
@@ -7,7 +7,10 @@ pub enum DecoderError {
     UnsupportedFunct3 { raw: u32, funct3: u8 },
     UnsupportedFunct7 { raw: u32, funct7: u8 },
     UnknownOpcode { raw: u32, opcode: u8 },
+    InvalidRegister { reg: u8 },
     InvariantViolation(''static str),
+    InvalidInstructionLength { word: u32 },
 }
 
+pub type ZkvmError = DecoderError;
 pub type DecodeResult<T> = Result<T, DecoderError>;
