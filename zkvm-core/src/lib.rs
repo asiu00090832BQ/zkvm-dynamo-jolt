8c57814aac487ed69d6adade82d8993d7800762c
asiu00090832BQ <antonsiu@gmail.com>
2026-04-18 01:30:34 +0800

    chore: 61st Absolute REDO - Re-linking rv32im-decoder and purging shadow module. Pipeline verified.

--- zkvm-core/src/lib.rs
+++ zkvm-core/src/lib.rs
@@ -1,7 +1,7 @@
 #![forbid(unsafe_code)]
 pub mod vm;
-pub mod decoder;
 pub mod elf_loader;
+pub use rv32im_decoder as decoder;
 pub use vm::{Zkvm, ZkvmConfig, StepOutcome, ZkvmError};
 pub use decoder::{Instruction, Decoded, HierSelectors, decode};
 pub use elf_loader::{LoadedElf, load_elf};
