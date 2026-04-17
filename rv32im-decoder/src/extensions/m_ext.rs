4372d84164513f436e8b1ae4666da9641db45019
asiu00090832BQ <antonsiu@gmail.com>
2026-04-18 03:21:11 +0800

    fix: manual scrub of m_ext.rs logic and naming

--- rv32im-decoder/src/extensions/m_ext.rs
+++ rv32im-decoder/src/extensions/m_ext.rs
@@ -1,60 +1,20 @@
-use crate::{funct3, funct7, opcode, rd, rs1, rs2};
+use crate::types::Instruction;
+use crate::m_extension;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum MInstruction {
-    Mul { rd: u8, rs1: u8, rs2: u8 },
-    Mulh { rd: u8, rs1: u8, rs2: u8 },
-    Mulhsu { rd: u8, rs1: u8, rs2: u8 },
-    Mulhu { rd: u8, rs1: u8, rs2: u8 },
-    Div { rd: u8, rs1: u8, rs2: u8 },
-    Divu { rd: u8, rs1: u8, rs2: u8 },
-    Rem { rd: u8, rs1: u8, rs2: u8 },
-    Remu { rd: u8, rs1: u8, rs2: u8 },
-}
+pub fn decode_m_instruction(word: u32) -> Option<Instruction> {
+    let funct7 = crate::util::funct7(word);
+    let funct3 = crate::util::funct3(word);
 
-pub fn decode_m_instruction(word: u32) -> Option<MInstruction> {
-    if opcode(word) != 0b0110011 || funct7(word) != 0b0000001 {
-        return None;
+    let op = m_extension::decode_m_extension(funct7, funct3)?;
+
+    match op {
+        m_extension::MExtensionOp::Mul => Some(Instruction.:Mul),
+        m_extension::MExtensionOp::Mulh => Some(Instruction.:Mulh),
+        m_extension::MExtensionOp::Mulhsu => Some(Instruction.:Mulhsu),
+        m_extension::MExtensionOp::Mulhu => Some(Instruction.:Mulhu),
+        m_extension::MExtensionOp::Div => Some(Instruction.:Div),
+        m_extension::MExtensionOp::Divu => Some(Instruction.:Divu),
+        m_extension::MExtensionOp::Rem => Some(Instruction.:Rem,
+        m_extension::MExtensionOp::Remu => Some(Instruction.:Remu),
     }
-
-    let instruction = match funct3(word) {
-        0b000 => MInstruction::Mul {
-            rd: rd(word),
-            rs1: rs1(word),
-            rs2: rs2(word),
-        },
-        0b001 => MInstruction::Mulh {
-            rd: rd(word),
-            rs1: rs1(word),
-            rs2: rs2(word),
-        },
-        0b010 => MInstruction::Mulhsu {
-            rd: rd(word),
-            rs1: rs1(word),
-            rs2: rs2(word),
-        },
-        0b011 => MInstruction::Mulhu {
-            rd: rd(word),
-            rs1: rs1(word),
-            rs2: rs2(word),
-        },
-        0b100 => MInstruction::Div {
-            rd: rd(word),
-            rs1: rs1(word),
-            rs2: rs2(word),
-        },
-        0b110 => MInstruction::Rem {
-            rd: rd(word),
-            rs1: rs1(word),
-            rs2: rs2(word),
-        },
-        0b111 => MInstruction::Remu {
-            rd: rd(word),
-            rs1: rs1(word),
-            rs2: rs2(word),
-        },
-        _ => return None,
-    };
-
-    Some(instruction)
 }
