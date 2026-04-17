da0bbcfec0b02048e92f49e4ac3e2efe40321938
asiu00090832BQ <antonsiu@gmail.com>
2026-04-18 03:11:53 +0800

    infra: Absolute REDO - src/lib.rs

--- rv32im-decoder/src/lib.rs
+++ rv32im-decoder/src/lib.rs
@@ -1,16 +1,22 @@
+#![no_std]
 #![forbid(unsafe_code)]
 
-pub mod base_i;
-pub mod decoder;
 pub mod error;
 pub mod formats;
 pub mod instruction;
-pub mod invariants;
+pub mod base_i;
 pub mod m_extension;
+pub mod invariants;
+pub mod decode;
 
-pub use decoder::decode_word;
-pub use error:{DecodeResult, DecoderError};
-pub use instruction::{DecodedInstruction, MInstruction};
+pub use crate::error::DecodeError;
+pub use crate::formats:{
+    fence_fm, fence_pred, fence_succ, funct3, funct7, imm12, imm_b, imm_i, imm_j, imm_s, imm_u,
+    is_32bit, opcode, rd, rs1, rs2, shamt,
+};
+pub use crate::instruction:;
 
-#[cfg(test)]
-mod tests;
+[inline]
+pub fn decode(word: u32) -> Result<Instruction, DecodeError> {
+    instruction::Instruction::decode(word)
+}
