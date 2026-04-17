fe5c8830fdc233957e0489beed67eac8d8944627
asiu00090832BQ <antonsiu@gmail.com>
2026-04-18 02:45:36 +0800

    infrastructure: export correctly from decoder crate

--- zkvm-core/src/lib.rs
+++ zkvm-core/src/lib.rs
@@ -4,6 +4,6 @@ pub mod vm;
 pub mod elf_loader;
 pub mod proof;
 
-pub use vm::{Zkvm, ZkvmConfig, StepOutcome, ZkvmError};
+pub use vm::{Zkvm, ZkvmError, ZkxmConfig, StepOutcome};
 pub use rv32im_decoder::{DecodedInstruction as Instruction, MInstruction};
 pub use elf_loader::{LoadedElf, load_elf};
