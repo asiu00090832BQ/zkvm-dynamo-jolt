302564b1eca26607f0e182505e85d3325997cb95
asiu00090832BQ <antonsiu@gmail.com>
2026-04-18 02:46:05 +0800

    Absolute REDO: Stabilize zkvm-core/src/lib.rs

--- zkvm-core/src/lib.rs
+++ zkvm-core/src/lib.rs
@@ -1,9 +1,10 @@
 #![forbid(unsafe_code)]
 
 pub mod vm;
+pub mod decoder;
 pub mod elf_loader;
 pub mod proof;
 
-pub use vm::{Zkvm, ZkvmError, ZkxmConfig, StepOutcome};
-pub use rv32im_decoder::{DecodedInstruction as Instruction, MInstruction};
+pub use vm::{Zkvm, ZkvmConfig, StepOutcome, ZcvmError as ZkvmError};
+pub use decoder::{Instruction, Decoded, HierSelectors, decode};
 pub use elf_loader::{LoadedElf, load_elf};
