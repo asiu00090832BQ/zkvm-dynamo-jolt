ec92ed5ebc79d676cdc25476d01ca8058988ef30
asiu00090832BQ <antonsiu@gmail.com>
2026-04-18 02:58:18 +0800

    infrastructure: update zkvm-core/src/lib.rs to use rv32im_decoder crate

--- zkvm-core/src/lib.rs
+++ zkvm-core/src/lib.rs
@@ -1,7 +1,6 @@
 #![forbid(unsafe_code)]
 pub mod vm;
-pub mod decoder;
 pub mod elf_loader;
 pub use vm::{Zkvm, ZkvmConfig, StepOutcome, ZkvmError};
-pub use decoder::{Instruction, Decoded, HierSelectors, decode};
+pub use rv32im_decoder::{Instruction, Decoded, HierSelectors, decode};
 pub use elf_loader::{LoadedElf, load_elf};
