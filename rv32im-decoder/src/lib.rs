7d3f97b1dff2bdb15e57366c95990bfac4d25c5e
asiu00090832BQ <antonsiu@gmail.com>
2026-04-17 23:48:52 +0800

    feat: update lib.rs for modular rv32im-decoder

--- rv32im-decoder/src/lib.rs
+++ rv32im-decoder/src/lib.rs
@@ -1,8 +1,12 @@
-pub mod instruction;
+#![no_std]
+#![forbid(unsafe_code)]
+
 pub mod decoder;
+pub mod instruction;
 pub mod m_extension;
 pub mod selectors;
 
-pub use instruction::Instruction;
 pub use decoder::{decode, DecodeError};
+pub use instruction::Instruction;
+pub use m_extension::decode_m_extension;
 pub use selectors::DecodeSelectors;
