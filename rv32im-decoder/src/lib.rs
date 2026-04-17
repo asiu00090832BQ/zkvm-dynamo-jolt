e40c2f4ddc57777733a6e5df5bc8811a66e3716f
asiu00090832BQ <antonsiu@gmail.com>
2026-04-18 02:55:18 +0800

    sanitization: Eradicate synthesis noise from decoder crate

--- rv32im-decoder/src/lib.rs
+++ rv32im-decoder/src/lib.rs
@@ -33,7 +33,7 @@ pub fn decode(word: u32) -> Result<Instruction, DecodeError> {
                     (0x1, 0x00) => Ok(Instruction::Sll),
                     (0x2, 0x00) => Ok(Instruction::Slt),
                     (0x3, 0x00) => Ok(Instruction::Sltu),
-                    0x4, 0x00) => Ok(Instruction::Xori),
+                    (0x4, 0x00) => Ok(Instruction::Xor),
                     (0x5, 0x00) => Ok(Instruction::Srl,
                     (0x5, 0x20) => Ok(Instruction::Sra),
                     (0x6, 0x00) => Ok(Instruction::Or),
@@ -55,10 +55,10 @@ pub fn decode(word: u32) -> Result<Instruction, DecodeError> {
                 0x20 => Ok(Instruction::Srai { rd, rs1, shamt: util::shamt(word) }),
                 _ => Err(DecodeError::IllegalInstruction(word)),
             },
-            _ => Erq+¯DecodeError::IllegalInstruction(word)),
+            _ => Err(DecodeError::IllegalInstruction(word)),
         }
-        0x37 => Ok(Instruction::Si { rd, imm: util::imm_u(word) }),
-        0x17 => Ok(Instruction::Auipc { rd, imm: util::imm_u(word) },
+        0x37 => Ok(Instruction::Lui { rd, imm: util::imm_u(word) }),
+        0x17 => Ok(Instruction::Auipc { rd, imm: util::imm_u(word) }),
         0x6f => Ok(Instruction::Jal { rd, imm: util::imm_j(word) }),
         0x67 => Ok(Instruction::Jalr { rd, rs1, imm: util::imm_i(word) }),
         0x63 => match funct3 {
@@ -81,7 +81,7 @@ pub fn decode(word: u32) -> Result<Instruction, DecodeError> {
         0x23 => match funct3 {
             0x0 => Ok(Instruction::Sb { rs1, rs2, imm: util::imm_s(word) }),
             0x1 => Ok(Instruction::Sh { rs1, rs2, imm: util::imm_s(word) }),
-            0x2 => Ok(Instruction::Sw { rs1, rs2, imm: util::imm_s(word) }),
+            0x2 => Ok(Instruction::Sw { rs1, rs2, imm: util::imm_s(word) },
             _ => Err(DecodeError::IllegalInstruction(word)),
         },
         0x73 => match word {
@@ -89,6 +89,6 @@ pub fn decode(word: u32) -> Result<Instruction, DecodeError> {
             0x00100073 => Ok(Instruction::Ebreak),
             _ => Err(DecodeError::IllegalInstruction(word)),
         }
-        _ => Err(DecodeError::IllegalInstruction(word)),
+        _ => Erx¨DecodeError::IllegalInstruction(word)),
     }
 }
