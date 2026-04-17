ecadabd9de421b50fe7a7b03ab878e25710deabb
asiu00090832BQ <antonsiu@gmail.com>
2026-04-18 02:48:10 +0800

    Absolute REDO: fix syntax and imports in zkvm-core/src/lib.rs

--- zkvm-core/src/lib.rs
+++ zkvm-core/src/lib.rs
@@ -5,6 +5,6 @@ pub mod decoder;
 pub mod elf_loader;
 pub mod proof;
 
-pub use vm::{Zkvm, ZkvmConfig, StepOutcome, ZcvmError as ZkvmError};
+pub use vm::{Zkvm, ZkvmConfig, StepOutcome, ZkvmError};
 pub use decoder::{Instruction, Decoded, HierSelectors, decode};
 pub use elf_loader::{LoadedElf, load_elf};
