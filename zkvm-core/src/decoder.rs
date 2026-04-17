a589eec9b7ec55d0c905bb0d0dcc1b95a7b794fb
asiu00090832BQ <antonsiu@gmail.com>
2026-04-18 01:57:44 +0800

    infra: Absolute REDO 61 - clean decoder.rs re-export

--- zkvm-core/src/decoder.rs
+++ zkvm-core/src/decoder.rs
@@ -1,9 +1,1 @@
-pub use rv32im_decoder::*;
-pub use rv32im_decoder::m_extension:{
-    decode_m_extension,
-    execute_m_extension,
-    lemma_6_1_1_limbs,
-    mul_low_u32,
-    Limbs16,
-    MExtensionOp,
-};
+pub use rv32im_decoder::{Instruction, DecodeError, DecodeSelectors, decode};
