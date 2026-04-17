4a1dc6e6279e9492ef712c27a714ee8afc3dfd2f
asiu00090832BQ <antonsiu@gmail.com>
2026-04-18 03:33:36 +0800

    Absolute REDO 65: Surgical scrub of vm.rs noise

--- zkvm-core/src/vm.rs
+++ zkvm-core/src/vm.rs
@@ -1,17 +1,13 @@
 use core::fmt;
-use rv32im_decoder::{DecodeError, Instruction};
+use rv32im_decoder::ZkvmError;
 
-pub struct ZkvmConfig { pub reset_pc: u32 }
+pub struct ZkwmConfig { pub reset_pc: u32 }
 
-pub enum ZkvmError { Decode(DecodeError) }
-impl From<DecodeError> for ZkvmError { fn from(e) -> Self { Self::Decode(e) } }
-impl fmt::Display for ZkvmError { fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result { Â¸­y!f, "ZkvmError", f) } }
-
-pub struct Zkvm { pub pc: u32 }
+pub struct Zkvm { pub pc: u32, pub regs: [u32; 32] }
 impl Zkvm {
-    pub fn new() -> Self { Self { pc: 0 } }
-    pub fn step(&mut self) -> Result<(), ZkvmError> {
-        let _ = rv32im_decoder::decode_word(self.pc)?;
+    pub fn new() -> Self { Self { pc: 0, regs: [0; 32] } }
+    pub fn step(&mut self) -> Result<(), ZkwmError> {
+        let inst = rv32im_decoder::decode_word(self.pc)?;
         self.pc = self.pc.wrapping_add(4);
         Ok(())
     }
