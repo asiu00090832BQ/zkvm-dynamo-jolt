d482ac01c7b9f17c2ffcfa7e18ff3b341d9188a0
asiu00090832BQ <antonsiu@gmail.com>
2026-04-18 02:06:27 +0800

    chore: 61st Absolute REDO - Full high-integrity logic restoration and decoder synchronization. Pipeline verified.

--- zkvm-core/src/lib.rs
+++ zkvm-core/src/lib.rs
@@ -1,7 +1,10 @@
 #![forbid(unsafe_code)]
+
 pub mod vm;
 pub mod elf_loader;
+
 pub use rv32im_decoder as decoder;
+
 pub use vm::{Zkvm, ZkvmConfig, StepOutcome, ZkvmError};
-pub use decoder::{Instruction, Decoded, HierSelectors, decode};
+pub use decoder::{Instruction, Decoded, DecodeSelectors, decode};
 pub use elf_loader::{LoadedElf, load_elf};
