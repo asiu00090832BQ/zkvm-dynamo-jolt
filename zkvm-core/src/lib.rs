2619f2120fddcdf03fc4bb52e39c4dee353f2e84
asiu00090832BQ <antonsiu@gmail.com>
2026-04-18 02:42:44 +0800

    infrastructure: align zkvm-core/lib.rs with modular rv32im-decoder

--- zkvm-core/src/lib.rs
+++ zkvm-core/src/lib.rs
@@ -1,7 +1,9 @@
 #![forbid(unsafe_code)]
+
 pub mod vm;
-pub mod decoder;
 pub mod elf_loader;
+pub mod proof;
+
 pub use vm::{Zkvm, ZkvmConfig, StepOutcome, ZkvmError};
-pub use decoder::{Instruction, Decoded, HierSelectors, decode};
+pub use rv32im-decoder = { DecodedInstruction as Instruction, MInstruction, decode_word as decoder };
 pub use elf_loader::{LoadedElf, load_elf};
