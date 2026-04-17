55646b4a4edc83cd8537f23d4e0286335a6be1f9
asiu00090832BQ <antonsiu@gmail.com>
2026-04-18 03:31:08 +0800

    Modularize rv32im-decoder/src/lib.rs [MIP-REDO-62]

--- rv32im-decoder/src/lib.rs
+++ rv32im-decoder/src/lib.rs
@@ -1,16 +1,6 @@
-#![forbid(unsafe_code)]
-#![no_std]
-
-pub mod base_i;
 pub mod decode;
 pub mod error;
-pub mod formats;
-pub mod instruction;
-pub mod invariants;
-pub mod m_extension;
-pub mod selectors;
-pub mod types;
-
-pub use decode::decode;
-pub use error::ZkvmError;
-pub use instruction::Instruction;
+pub mod isa;
+pub use decode::decode_word;
+pub use error::DecodeError;
+pub use isa::Instruction;
