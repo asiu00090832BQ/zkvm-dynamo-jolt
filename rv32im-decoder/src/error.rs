d38fd828970f5e7922865e4b3cdd8be8b5582f25
asiu00090832BQ <antonsiu@gmail.com>
2026-04-18 03:29:20 +0800

    infrastructure: delete redundant error.rs

--- rv32im-decoder/src/error.rs
+++
@@ -1,9 +0,0 @@
-[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum ZkvmError {
-    InvalidOpcode(u32),
-    InvalidFunct3(u32),
-    InvalidFunct7(u32),
-    MalformedInstruction(u32),
-}
-
-pub type ZkvmResult<T> = Result<T, ZkvmError>;
