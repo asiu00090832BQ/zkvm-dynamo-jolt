f2212dbaef0a9660494b79355ad3b092f36a57f9
asiu00090832BQ <antonsiu@gmail.com>
2026-04-18 02:52:18 +0800

    refactor(rv32im-decoder): normalize exports and remove core coupling

--- rv32im-decoder/src/lib.rs
+++ rv32im-decoder/src/lib.rs
@@ -1,3 +1,16 @@
-pub enum Instruction { Add { rd: u8, rs1: u8, rs2: u8 } }
-pub enum DecodeError { Invalid(u32) }
-pub fn decode_instruction(_: u32) -> Result<Instruction, DecodeError> { Ok(Instruction::Add { rd: 0, rs1: 0, rs2: 0 }) }
+#![forbid(unsafe_code)]
+
+pub mod decode;
+pub mod error;
+pub mod formats;
+pub mod instruction;
+pub mod m_extension;
+pub mod base_i;
+
+pub use decode::{decode, decode_instruction};
+pub use error::DecoderError;
+pub use formats::{BType, IType, JType, RType, SType, ShiftIType, UType};
+pub use instruction::Instruction;
+pub use m_extension::{
+    decompose_u32, div, divu, mul, mulh, mulhsu, mulhu, plan_mul_limbs, rem, remu, Limb16,
+};
