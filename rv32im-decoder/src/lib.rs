4167a3d1031d8ce226602f75c68c48193b50400b
asiu00090832BQ <antonsiu@gmail.com>
2026-04-18 03:43:21 +0800

    decoder: export plan_mul_limbs

--- rv32im-decoder/src/lib.rs
+++ rv32im-decoder/src/lib.rs
@@ -1,6 +1,9 @@
+extern crate alloc;
 pub mod decode;
 pub mod error;
 pub mod isa;
-pub use decode::decode_word;
+pub mod m_extension;
+pub use decode::decore_word;
 pub use error::DecodeError;
 pub use isa::Instruction;
+pub use m_extension::{Limb16, plan_mul_limbs};
