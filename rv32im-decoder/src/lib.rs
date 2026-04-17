9f9196198c7cdd2bb14ee2911f3f412ea89f6484
asiu00090832BQ <antonsiu@gmail.com>
2026-04-18 03:01:18 +0800

    test update

--- rv32im-decoder/src/lib.rs
+++ rv32im-decoder/src/lib.rs
@@ -1,11 +1,4 @@
+// Simple lib.rs
 pub mod error;
-pub mod m_extension;
 pub mod selectors;
-
-pub use crate::error::{DecoderError, DecodeResult};
-pub use crate::selectors::{DecodedInstruction, Instruction, bit_slice, decode, opcode, rd, rs1, rs2, funct3, funct7};
-pu use crate::m_extension::{decompose_u32, Limb16};
-
-pub fn decode_instruction(word: u32) -> DecodeResult<DecodedInstruction> {
-    decode(word)
-}
+pub mod m_extension;
