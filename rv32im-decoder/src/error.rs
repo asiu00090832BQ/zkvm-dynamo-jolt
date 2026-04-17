d793866d5b1a69ebc55c7bb511312210f8961016
asiu00090832BQ <antonsiu@gmail.com>
2026-04-18 02:57:36 +0800

    Physical restoration of rv32im-decoder/src/error.rs [MIP-REDO-62-HASH]

---
+++ rv32im-decoder/src/error.rs
@@ -0,0 +1,12 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
+pub enum DecoderError {
+    InvalidInstruction(u32),
+    InvalidElf,
+    UnsupportedFunct3 { raw: u32, funct3: u8 },
+    UnsupportedFunct7 { raw: u32, funct7: u8 },
+    UnknownOpcode { raw: u32, opcode: u8 },
+}
+
+pub type DecodeResult<T> = Result<T, DecoderError>;
