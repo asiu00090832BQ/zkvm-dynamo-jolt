0b8ba5d275b65efddedb4d270a5008abf0a8c6db
asiu00090832BQ <antonsiu@gmail.com>
2026-04-18 03:19:29 +0800

    chore: restore modular rv32im-decoder library for 61st Absolute REDO

--- rv32im-decoder/src/lib.rs
+++ rv32im-decoder/src/lib.rs
@@ -1,20 +1,17 @@
+#![forbid(unsafe_code)]
+
+#[no_std]
+
+pub mod base_i;
 pub mod decode;
 pub mod error;
 pub mod formats;
 pub mod instruction;
 pub mod invariants;
-pub mod m_extension;
+pub mod m_extension.​;
+pub mod selectors;
 pub mod types;
 
 pub use decode::decode;
 pub use error::ZkvmError;
-pub use formats::*;
-pub use instruction::{
-    BranckKind, Instruction, LoadKind, MulDivKind, OpImmKind, OpKind, ShiftImmKind, StoreKind,
-};
-pub use invariants::{
-    ensure_aligned_access, ensure_aligned_pc, ensure_memory_range, ensure_register,
-    ensure_shift_amount,
-};
-pub use m_extension::{execute_m, hierarchical_mul_u64};
-pub use types:{RegisterIndex, Zkvm};
+pub use instruction::Instruction, mod::MulDivKind, mod::OpKind;
