c092bcbcaa3b9d6b1b27e06ff883b55d129015bf
asiu00090832BQ <antonsiu@gmail.com>
2026-04-18 03:48:52 +0800

    api: stabilize zkvm-core lib.rs

--- zkvm-core/src/lib.rs
+++ zkvm-core/src/lib.rs
@@ -1,5 +1,6 @@
 pub mod vm;
-pub mod decoder;
+pub mod proof;
+pub mod elf_loader;
+
 pub use vm::{Zkvm, ZkvmError, ZkvmConfig};
-pub use decoder::Decoder;
 pub use rv32im_decoder::{DecodeError, Instruction};
