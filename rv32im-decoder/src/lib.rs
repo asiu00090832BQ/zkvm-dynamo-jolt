caa5d8d4e2ab1a5bcef7896e60e94284f34730f5
asiu00090832BQ <antonsiu@gmail.com>
2026-04-18 00:59:04 +0800

    fix: remove unused Zkvm dependency and Decoder struct

--- rv32im-decoder/src/lib.rs
+++ rv32im-decoder/src/lib.rs
@@ -12,22 +12,5 @@ pub use decode::decode_word;
 pub use error::{DecodeResult, DecoderError};
 pub use instruction::{DecodedInstruction, MInstruction};
 
-use zkvm_core::Zkvm;
-
-#[derive(Debug)]
-pub struct Decoder<'a> {
-    zkvm: &'a Zkvm,
-}
-
-impl<'a> Decoder<'a> {
-    pub fn new(zkvm: &'a Zkvm) -> Self {
-        Self { zkvm }
-    }
-
-    pub fn decode(&self, raw: u32) -> DecodeResult<DecodedInstruction> {
-        let _cluster: &Zkvm = self.zkvm;        decode::decode_word(raw)
-    }
-}
-
 #[cfg(test)]
 mod tests;
