b581146b413e14eb12aeae9925be6fcc58aed1eb
asiu00090832BQ <antonsiu@gmail.com>
2026-04-18 01:47:12 +0800

    chore: update rv32im-decoder lib.rs

--- rv32im-decoder/src/lib.rs
+++ rv32im-decoder/src/lib.rs
@@ -1,3 +1,5 @@
-pub enum Instruction { Add { rd: u8, rs1: u8, rs2: u8 } }
-pub enum DecodeError { Invalid(u32) }
-pub fn decode_instruction(_: u32) -> Result<Instruction, DecodeError> { Ok(Instruction::Add { rd: 0, rs1: 0, rs2: 0 }) }
+pub mod types;
+pub mod decoder;
+
+pub use types:{Instruction, DecodeError};
+pub use decoder::decode;
