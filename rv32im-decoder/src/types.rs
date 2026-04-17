0b93c025db3a445e33892d6f51451a728755d456
asiu00090832BQ <antonsiu@gmail.com>
2026-04-18 01:46:43 +0800

    feat: add clean types.rs to rv32im-decoder

---
+++ rv32im-decoder/src/types.rs
@@ -0,0 +1,24 @@
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Instruction {
+    Add { rd: usize, rs1: usize, rs2: usize },
+    Sub { rd: usize, rs1: usize, rs2: usize },
+    Mul { rd: usize, rs1: usize, rs2: usize },
+    Mulh { rd: usize, rs1: usize, rs2: usize },
+    Mulhsu { rd: usize, rs1: usize, rs2: usize },
+    Mulhu { rd: usize, rs1: usize, rs2: usize },
+    Div { rd: usize, rs1: usize, rs2: usize },
+    Divu { rd: usize, rs1: usize, rs2: usize },
+    Rem { rd: usize, rs1: usize, rs2: usize },
+    Remu { rd: usize, rs1: usize, rs2: usize },
+    Addi { rd: usize, rs1: usize, imm: i32 },
+    Lui { rd: usize, imm: i32 },
+    Jal { rd: usize, imm: i32 },
+    Ecall,
+    Ebreak,
+    Invalid(u32),
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum DecodeError {
+    InvalidWord(u32),
+}
