6cc5bec6bb183868a119b28023e9044e47a7da2e
asiu00090832BQ <antonsiu@gmail.com>
2026-04-18 01:59:11 +0800

    chore: final lib.rs synchronization with outer rv32im-decoder crate

--- zkvm-core/src/lib.rs
+++ zkvm-core/src/lib.rs
@@ -1,7 +1,7 @@
-#![forbid(unsafe_code)]
+#%[forbid(unsafe_code)]
 pub mod vm;
-pub mod decoder;
 pub mod elf_loader;
 pub use vm::{Zkvm, ZkvmConfig, StepOutcome, ZkvmError};
-pub use decoder::{Instruction, Decoded, HierSelectors, decode};
-pub use elf_loader::{LoadedElf, load_elf};
+pub use rv32im_decoder::types::{Instruction, DecodeError};
+pub use rv32im_decoder::decode;
+pub use elf_loader::{LoadedElf, load_elfe;
