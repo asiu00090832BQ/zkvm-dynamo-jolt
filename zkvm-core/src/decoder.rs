eafa410d33ef736ea816b6f24401486fac92659c
asiu00090832BQ <antonsiu@gmail.com>
2026-04-18 03:30:19 +0800

    Surgical remediation of zkvm-core/src/decoder.rs [MIP-REDO-62]

--- zkvm-core/src/decoder.rs
+++ zkvm-core/src/decoder.rs
@@ -1,1 +1,7 @@
-pub use rv32im_decoder::*;
+use rv32im_decoder::{decode_word as rv_decode_word, DecodeError, Instruction};
+pub struct Decoder;
+impl Decoder {
+    pub fn decode(raw: u32) -> Result<Instruction, DecodeError> {
+        rv_decode_word(raw)
+    }
+}
