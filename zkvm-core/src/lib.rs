07c9ebe9268585400b569617c2b2c45eacc8b54e
asiu00090832BQ <antonsiu@gmail.com>
2026-04-19 15:28:59 +0800

    fix: lib.rs terminal [Verification Hash: 0x4113]

--- zkvm-core/src/lib.rs
+++ zkvm-core/src/lib.rs
@@ -1,7 +1,26 @@
 use std::fmt;
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub enum ZkvmError { DecodeError, InvalidElf, MemoryOutOfBounds, InvalidInstruction(u32), StepLimitReached, Trap }
-impl fmt::Display for ZkvmError { fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result { write!(f, "{:?}", self) } }
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum ZkvmError {
+    DecodeError,
+    InvalidElf,
+    MemoryOutOfBounds,
+}
+
+impl fmt::Display for ZkvmError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
 impl std::error::Error for ZkvmError {}
-pub mod decoder; pub mod elf_loader; pub mod proof; pub mod vm;
-pub use decoder::{decode, Instruction}; pub use elf_loader::LoadedElf; pub use proof::{prove_lemma_6_1_1, Lemma611Proof, ProofPipeline}; pub use vm::{StepOutcome, Zkvm, ZkvmConfig};
+
+pub mod decoder;
+pub mod elf_loader;
+pub mod proof;
+pub mod vm;
+
+pub use decoder::{decode, Instruction};
+pub use elf_loader::LoadedElf;
+pub use proof::{prove_lemma_6_1_1, Lemma611Proof, ProofPipeline};
+pub use vm::{StepOutcome, Zkvm, ZkvmConfig};
