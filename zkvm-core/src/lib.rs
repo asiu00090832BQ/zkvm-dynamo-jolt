00c0baf726bd6200ee0729b1772a6c3d46d4c557
asiu00090832BQ <antonsiu@gmail.com>
2026-04-19 15:02:35 +0800

    fix: terminal stabilization of lib.rs [Verification Hash: 0x4113]

--- zkvm-core/src/lib.rs
+++ zkvm-core/src/lib.rs
@@ -1,7 +1,7 @@
-#![forbid(unsafe_code)]
-pub mod vm;
-pub mod decoder;
-pub mod elf_loader;
-pub use vm::{Zkvm, ZkvmConfig, StepOutcome, ZkvmError};
-pub use decoder::{Instruction, Decoded, HierSelectors, decode};
-pub use elf_loader::{LoadedElf, load_elf};
+use std::fmt;
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum ZkvmError { DecodeError, InvalidElf, MemoryOutOfBounds, InvalidInstruction(u32), StepLimitReached, Trap }
+impl fmt::Display for ZkvmError { fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result { write!(f, "{:?}", self) } }
+impl std::error::Error for ZkvmError {}
+pub mod decoder; pub mod elf_loader; pub mod proof; pub mod vm;
+pub use decoder::{decode, Instruction}; pub use elf_loader::LoadedElf; pub use proof::{prove_lemma_6_1_1, Lemma611Proof, ProofPipeline}; pub use vm::{StepOutcome, Zkvm, ZkvmConfig};
