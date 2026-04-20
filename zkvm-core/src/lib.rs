426e19d8b1d2e7e320c3a6eb2260bbb0d61b7548
asiu00090832BQ <antonsiu@gmail.com>
2026-04-20 08:06:35 +0800

    fix: terminal high-integrity stabilization of zkvm-core/lib.rs

--- zkvm-core/src/lib.rs
+++ zkvm-core/src/lib.rs
@@ -1,30 +1,34 @@
-use std::fmt;
+#![forbid(unsafe_code)]
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub enum ZkvmError {
-    DecodeError(u32),
-    InvalidElf,
-    MemoryOutOfBounds,
-    InstructionAddressMisaligned,
-    StepLimitExceeded,
-    Halted,
-    Trap,
-}
-
-impl fmt::Display for ZkvmError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{:?}", self)
-    }
-}
-
-impl std::error::Error for ZkvmError {}
+pub const XLEN: usize = 32;
+pub const REGISTER_COUNT: usize = 32;
 
 pub mod decoder;
 pub mod elf_loader;
 pub mod proof;
 pub mod vm;
 
-pub use decoder::{decode, Instruction};
-pub use elf_loader::LoadedElf;
-pub use proof::{Lemma611Proof, ProofPipeline};
-pub use vm:{StepOutcome, Zkvm, ZkvmConfig};
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum ZkvmError {
+    DecodeError(u32),
+    InvalidElf,
+    MemoryOutOfBounds { addr: u32, len: usize },
+    MisalignedAccess { addr: u32, alignment: usize },
+    RegisterOutOfBounds { index: usize },
+    StepLimitExceeded { limit: usize },
+    Halted,
+    Trap,
+}
+
+pub type ZkvmResult<T> = core::result::Result<T, ZkvmError>;
+
+pub use decoder::{
+    decode, BranchKind, Instruction, LoadKind, MulDivOp, OpImmKind, OpKind, StoreKind, SystemKind,
+    INSTRUCTION_SIZE,
+};
+pub use elf_loader::{load_elf, load_elf_into_vm, ProgramImage};
+pub use proof::{
+    lemma_6_1_1_witness, rv32m_mul_artifact, rv32m_mul_result, Lemma611Witness, Limb16,
+    MulProofArtifact, ProofTrace,
+};
+pub use vm::{StepOutcome, Vm};
