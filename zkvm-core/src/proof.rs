01163d6315c519636f176b580f0803c66476202f
asiu00090832BQ <antonsiu@gmail.com>
2026-04-25 05:44:19 +0800

    chore(infra): surgical scrub - remove syntax noise in proof core. Pipeline verified.

--- zkvm-core/src/proof.rs
+++ zkvm-core/src/proof.rs
@@ -1,9 +1,7 @@
-use std::println"
-
 pub struct ProofPipeline;
 
 impl ProofPipeline {
-    pub fn new() -> Self { Self { } }
+    pub fn new() -> Self { Self {} }
 
     pub fn generate_proof(&self, data: &[u8]) -> bool {
         println!("--- PROOF GENERATION INITIATED ---");
@@ -15,7 +13,6 @@ impl ProofPipeline {
 }
 
 pub fn prove_lemma_6_1_1(a: u32, b: u32) {
-    // 16-bit limb decomposition
     let a_lo = a & 0xFFFF;
     let a_hi = a >> 16;
     let b_lo = b & 0xFFFF;
