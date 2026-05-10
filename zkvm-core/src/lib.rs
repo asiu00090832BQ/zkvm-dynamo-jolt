847358cf967b7d5f1fab849258ba71ec810367f6
asiu00090832BQ <antonsiu@gmail.com>
2026-05-11 02:45:00 +0800

    fix: export StepOutcome and load_elf from zkvm-core

--- zkvm-core/src/lib.rs
+++ zkvm-core/src/lib.rs
@@ -3,5 +3,6 @@ pub mod decoder;
 pub mod elf_loader;
 pub mod proof;
 
-pub use vm::{Zkvm, ZkvmError, ZkvmConfig};
+pub use vm::{Zkvm, ZkvmError, ZkvmConfig, StepOutcome};
 pub use decoder::*;
+pub use elf_loader::load_elf;
